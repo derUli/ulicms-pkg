5c703f83ae96ccec80f1ec1814779a0c7a97d334
Ulrich Schmidt <admin@deruli.de>
2013-08-04 19:53:59 +0200

    fullcalendar-0.0.1 hinzugefügt

---
+++ sources/fullcalendar-0.0.1/src/post-install.php
@@ -0,0 +1,12 @@
+<?php
+// post-install Script für fullcalendar
+db_query("CREATE TABLE IF NOT EXISTS `".tbname("events")."` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` text NOT NULL,
+  `start` bigint(20) NOT NULL,
+  `end` bigint(20) NOT NULL,
+  `url` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;");
+
+?>
