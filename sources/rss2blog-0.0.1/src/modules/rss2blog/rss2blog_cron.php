5205d896eaa0fc39e9f2029370c8b95aff76c72c
Ulrich Schmidt <u.schmidt@velian.de>
2013-08-05 16:42:07 +0200

    Code refakturiert

--- sources/rss2blog-0.0.1/src/modules/rss2blog/rss2blog_cron.php
+++ sources/rss2blog-0.0.1/src/modules/rss2blog/rss2blog_cron.php
@@ -85,6 +85,10 @@ if(!$cache_time)
 
 
 
+
+
+
+
 for($n = 0; $n < count($srclist); $n++){
      $currentLine = trim($srclist[$n]);
      if(!startsWith($currentLine, "#") and !empty($currentLine)){
