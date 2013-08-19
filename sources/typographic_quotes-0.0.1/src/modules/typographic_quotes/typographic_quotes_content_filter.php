66ac5a0d790847ce3571a7f076e2d6a98e167b7e
Ulrich Schmidt <admin@deruli.de>
2013-08-19 18:37:53 +0200

    Typografische Anführungaszeichen

---
+++ sources/typographic_quotes-0.0.1/src/modules/typographic_quotes/typographic_quotes_content_filter.php
@@ -0,0 +1,46 @@
+<?php 
+// Ersetze doppelte Anführungszeichen durch Typographische
+function typographic_quotes_content_filter($input){
+   $returnval = "";
+   $quote = false;
+   $tag_open = false;
+   $code_or_pre = false;
+   for($i=0; $i<strlen($input); $i++){
+   
+      switch($input[$i]){
+         case "\"": case "&quot;":
+         if($quote and !$tag_open and !$code_or_pre){
+            $returnval .= "“";
+            $quote = false;
+         } else if(!$quote and !$tag_open and !$code_or_pre){
+           $quote = true;
+           $returnval .= "„";
+         } else{
+           $returnval .= $input[$i];
+         }
+         break; break;
+         case "<":
+         $tag_open = true;
+         $returnval .= $input[$i];
+         break;
+         case ">":
+         $tag_open = false;
+         $returnval .= $input[$i];
+         break;
+         default:
+         $returnval .= $input[$i];
+         break;
+      }
+      
+      if(endsWith(strtolower($returnval), "<code>") or 
+         endsWith(strtolower($returnval), "<pre>"))
+         $code_or_pre = true;
+         
+     if(endsWith(strtolower($returnval), "</code>") or 
+        endsWith(strtolower($returnval), "</pre>"))
+         $code_or_pre = false;
+      
+   }
+   return $returnval;
+}
+?>
