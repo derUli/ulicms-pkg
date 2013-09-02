4962fdf30e3e0736b8673501c78a497385f48500
Ulrich Schmidt <u.schmidt@velian.de>
2013-09-02 16:30:36 +0200

    theme-touching: Entwicklung begonnen

---
+++ sources/theme-touching_design-0.0.1/src/templates/touching_design/oben.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<?php base_metas()?>
+<link rel="stylesheet" media="screen" type="text/css" href="<?php echo getTemplateDirPath("touching_design");
+?>style.css"/>
+
+</head>
+<body>
+ <!-- Generated at www.csscreator.com -->
+<div id="container">
+	<div id="banner">
+		<div id='bannertitle'><?php homepage_title();?></div>
+	</div>
+
+	<div id="outer">
+ 		<div id="inner">
+ 			<div id="left">
+  <div class="verticalmenu">
+  <?php menu("left");?>
+ </div> 
+
+   		</div>
+   		<div id="content">
