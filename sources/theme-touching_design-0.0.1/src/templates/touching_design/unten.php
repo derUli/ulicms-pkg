1cc5045dcf3745ca0300cd6c96849144b0e8471d
Ulrich Schmidt <u.schmidt@velian.de>
2013-09-02 16:39:27 +0200

    copyright Hinweis geändert

--- sources/theme-touching_design-0.0.1/src/templates/touching_design/unten.php
+++ sources/theme-touching_design-0.0.1/src/templates/touching_design/unten.php
@@ -1,7 +1,8 @@
-
+<p><?php random_banner()?></p>
 		</div><!-- end content -->
 		</div><!-- end inner -->
 	</div><!-- end outer -->
- 	<div id="footer"><h1><a href="http://freethoughts.ftmblog.com">copyrights&copy;FTM</a></h1></div>
+ 	<div id="footer"><h1>&copy; <?php year()?> by <?php homepage_owner()?> | Powered by UliCMS <?php echo cms_version();?></h1></div>
+ 	
 </div><!-- end container -->
 </body></html>
