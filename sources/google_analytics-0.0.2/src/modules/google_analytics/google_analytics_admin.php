b8d8455327681823827978eff97898d3e458a0b7
Ulrich Schmidt <admin@deruli.de>
2013-10-27 12:35:02 +0100

    GUI für Eingabe der Google Analytics ID hinzugefügt

---
+++ sources/google_analytics-0.0.2/src/modules/google_analytics/google_analytics_admin.php
@@ -0,0 +1,35 @@
+<?php
+define("MODULE_ADMIN_HEADLINE", "Google Analytics");
+
+$required_permission = getconfig("google_analytics_required_permission");
+
+if($required_permission === false){
+     $required_permission = 50;
+     }
+
+define(MODULE_ADMIN_REQUIRED_PERMISSION, $required_permission);
+
+
+function google_analytics_admin(){
+    
+     if(isset($_POST["submit"])){
+        setconfig("google_analytics_id", 
+        mysql_real_escape_string($_POST["google_analytics_id"]));
+      
+      }
+
+$ga_id = getconfig("google_analytics_id");
+if(!$ga_id)
+   $ga_id = "UA-XXXXX-YY";
+
+?>
+
+<form action="<?php echo getModuleAdminSelfPath()?>" method="post">
+<p>Web-Property-ID: <input name="google_analytics_id" type="text" value="<?php echo htmlspecialchars($ga_id);?>"></p>
+
+<p><input type="submit" name="submit" value="Einstellungen speichern"/></p>
+</form>
+<?php
+     }
+
+?>
