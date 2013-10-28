d0fbcb7bdb4d018d0c76c35e62266d596c42a655
Ulrich Schmidt <u.schmidt@velian.de>
2013-10-28 15:34:27 +0100

    jquery_tablesorter-2.0.3: Stylesheet eingebunden

--- sources/jquery_tablesorter-2.0.3/src/modules/jquery_tablesorter/jquery_tablesorter_head.php
+++ sources/jquery_tablesorter-2.0.3/src/modules/jquery_tablesorter/jquery_tablesorter_head.php
@@ -2,4 +2,5 @@
 if(containsModule(get_requested_pagename(), "jquery_tablesorter")){
 ?>
 <script type="text/javascript" src="<?php echo getModulePath("jquery_tablesorter");?>jquery.tablesorter.min.js"></script> 
+<link rel="stylesheet" type="text/css" href="<?php echo getModulePath("jquery_tablesorter");?themes/blue/style.css"/>
 <?php }?>
