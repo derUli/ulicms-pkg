2ba7eda36aad486bac579d43489e69aff31e40b6
Ulrich Schmidt <admin@deruli.de>
2013-11-18 18:04:25 +0100

    fullcalender 1.0.0 hinzugefügt

---
+++ sources/fullcalendar-1.0.0/src/json-events.php
@@ -0,0 +1,36 @@
+<?php
+ include "init.php";
+ $events = array();
+ 
+
+ 
+ if(!isset($_REQUEST["start"])){
+   $_REQUEST["start"] = mktime(0, 0, 0, date("m",$time), +1, date("y",$time)); 
+ }
+
+ if(!isset($_REQUEST["end"])){
+  $_REQUEST["end"] = mktime(0, 0, 0, date("m",$time)+1, 0, date("y",$time));
+}
+ 
+ $query = db_query("SELECT * FROM `" . tbname("events") . "` WHERE `start` >= ".intval($_REQUEST["start"]). " AND `end` <=". intval($_REQUEST["end"]). " ORDER BY id");
+ while($row = db_fetch_object($query)){
+     $obj = array();
+     $obj["id"] = $row -> id;
+    
+    
+    
+     $obj["start"] = date("Y-m-d", $row -> start);
+     $obj["end"] = date("Y-m-d", $row -> end);
+     $obj["title"] = $row -> title;
+    
+     if(!empty($row -> url) and $row -> url != "http://")
+         $obj["url"] = $row -> url;;
+    
+     array_push($events, $obj);
+    
+     }
+
+
+ echo json_encode($events);
+
+?>
