6ea8107d869b6c4933156a016eb1578fc185e3d0
Ulrich Schmidt <admin@deruli.de>
2013-11-19 18:13:01 +0100

    Komma vergessen

--- sources/placeholders-0.0.1/src/post-install.php
+++ sources/placeholders-0.0.1/src/post-install.php
@@ -6,7 +6,7 @@ db_query("CREATE TABLE IF NOT EXISTS `".tbname("placeholders")."` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `value` text NOT NULL,
-  `match_case` tinyint(1) NOT NULL DEFAULT '0'
+  `match_case` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;");
 
