ac0c4490ea853e1165e58ac6ca8bf5cc9ebe4976
Ulrich Schmidt <u.schmidt@velian.de>
2013-11-22 17:03:11 +0100

    post-install und init hinzugefügt

--- sources/block_ip-0.0.1/src/modules/block_ip/block_ip_init.php
+++ sources/block_ip-0.0.1/src/modules/block_ip/block_ip_init.php
@@ -1,5 +1,13 @@
 <?php 
 function isIPBlocked($ip){
     $blocked_ips = getconfig("blocked_ips");
+    if(!$blocked_ips)
+       return false;
+       
+     if(empty($blocked_ips))
+        return false;
+
+     $blocked_ips = str_replace("\r\n", "\n", $blocked_ips);
+     
     
     }
