460304487f8f406d39df3c2afbc40377fc508246
Ulrich Schmidt <u.schmidt@velian.de>
2013-11-22 17:07:50 +0100

    Typo

--- sources/block_ip-0.0.1/src/modules/block_ip/block_ip_init.php
+++ sources/block_ip-0.0.1/src/modules/block_ip/block_ip_init.php
@@ -10,7 +10,7 @@ function isIPBlocked($ip){
      $blocked_ips = str_replace("\r\n", "\n", $blocked_ips);
      $blocked_ips = explode("\n", $blocked_ips);
      
-     for($i = 0; $ i < count($blocked_ips ); $i++){
+     for($i = 0; $i < count($blocked_ips ); $i++){
         if(endsWith($blocked_ips[$i], ".")){
            if(startsWith($ip, $blocked_ips[$i]))
               return true;
