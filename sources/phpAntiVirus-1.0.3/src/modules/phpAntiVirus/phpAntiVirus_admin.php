a91417d650dab1246911c493e36da4a16cc651ce
derUli <admin@deruli.de>
2013-11-23 13:52:43 +0100

    phpAntiVirus Package hinzugefuegt

---
+++ sources/phpAntiVirus-1.0.3/src/modules/phpAntiVirus/phpAntiVirus_admin.php
@@ -0,0 +1,23 @@
+<?php
+define("MODULE_ADMIN_HEADLINE", "Anti-Virus");
+define("MODULE_ADMIN_REQUIRED_PERMISSION", "phpAntiVirus");
+
+function phpAntiVirus_admin(){
+     
+        
+        
+        
+         ?>
+
+<form action="<?php echo getModuleAdminSelfPath()?>" method="post">
+<input type="submit" name="submit" value="Scan durchführen"/>
+</form>
+<?php
+if(isset($_POST["submit"])){
+   include_once getModulePath("phpAntiVirus")."index.php";
+}
+
+
+         }
+    
+     ?>
