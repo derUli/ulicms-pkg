c5e08d8c17a16d22aa7fe9c62cbe76c58edbd674
Ulrich Schmidt <admin@deruli.de>
2013-11-25 20:20:00 +0100

    Theme "Under the Bridge" hinzugefügt

--- beta-packages.bat
+++ beta-packages.bat
@@ -1,2 +1,3 @@
 ulicms-pkg.py clean
 ulicms-pkg.py build theme-simple_beauty-0.0.1
+ulicms-pkg.py build theme-under_the_bridge-0.0.1
---
+++ sources/theme-under_the_bridge-0.0.1/src/templates/under_the_bridge/unten.php
@@ -0,0 +1,10 @@
+
+</div>
+
+
+<div id="footer">
+<p>&copy; <?php homepage_title();?> | by <?php homepage_owner();?> <?php year();?>| Valid Xhtml 1.0 Strict! </p>
+</div>
+</div>
+</body>
+</html>
