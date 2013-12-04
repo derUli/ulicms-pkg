ff7aaa3917944f0026c4a6bf6090aa4250148d95
Ulrich Schmidt <admin@deruli.de>
2013-12-04 19:03:52 +0100

    Basis dePaket unews hinzugefügt

---
+++ sources/unews-1.0.0/src/unews/unews_custom_acl.php
@@ -0,0 +1,5 @@
+<?php 
+global $acl_array;
+$acl_array["unews_news"] = null;
+$acl_array["unews_comments"] = null;
+?>
---
+++ sources/unews-1.0.0/src/unews/unews_views.php
@@ -0,0 +1,10 @@
+<?php 
+function unews_single_view($article){
+   return "Hier sollte die Seite ".$article." erscheinen.";
+}
+
+function unews_list_view($article){
+   return "Hier sollte ein Archiv der Artikel erscheinen";
+}
+
+?>
