5536c9c317ae4d9e4410360f3381491362fc593e
Ulrich Schmidt <admin@deruli.de>
2014-02-24 23:36:46 +0100

    trim

--- sources/block_ips-0.0.2/src/post-install.php
+++ sources/block_ips-0.0.2/src/post-install.php
@@ -6,6 +6,8 @@ $blocked_ips = str_replace(" ", "", $blocked_ips);
 
 $blocked_ips = str_replace(",", "\n", $blocked_ips);
 
+$blocked_ips = trim($blocked_ips);
+
 if(!getconfig("blocked_ips"))
      setconfig("blocked_ips", $blocked_ips);
 
