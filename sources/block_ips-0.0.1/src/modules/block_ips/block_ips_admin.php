54f954eb55db67d70976f0b14b9d6ca50845662b
Ulrich Schmidt <admin@deruli.de>
2014-02-24 23:50:47 +0100

    block_ips wieder entfernt, da es zu groß war.

---
+++ sources/block_ips-0.0.1/src/modules/block_ips/block_ips_admin.php
@@ -0,0 +1,31 @@
+<?php
+define("MODULE_ADMIN_HEADLINE", "Ip-Adressen blockieren");
+define("MODULE_ADMIN_REQUIRED_PERMISSION", "block_ips");
+
+function block_ips_admin(){
+    
+     if(isset($_POST["submit"])){
+         setconfig("blocked_ips",
+             db_escape($_POST["blocked_ips"]));
+        
+         }
+    
+    $blocked_ips = getconfig("blocked_ips");
+    $blocked_ips = stringHelper :: real_htmlspecialchars($blocked_ips);
+    ?>
+
+<form action="<?php echo getModuleAdminSelfPath()?>" method="post">
+<p>Hier können Sie den Zugriff von bestimmten IP-Adressen auf das System blockieren.<br/>
+Sie können je einen Eintrag pro Zeile eingeben.
+<br/>
+Wenn eine Zeile mit einem Punkt endet, werden alle IP-Adressen die damit anfangen blockiert.</p>
+<p>
+<textarea rows="10" cols="40" name="blocked_ips"><?php echo $blocked_ips;
+    ?></textarea>
+
+<p><input type="submit" name="submit" value="Einstellungen speichern"/></p>
+</form>
+<?php
+     }
+
+?>
---
+++ sources/block_ips-0.0.1/src/post-install.php
@@ -0,0 +1,4 @@
+<?php
+if(!getconfig("blocked_ips"))
+     setconfig("blocked_ips", "");
+
