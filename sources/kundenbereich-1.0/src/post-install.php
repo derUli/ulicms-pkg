bad2d47c3d49ff99dda6fa6e5c7f04b27b238bde
Ulrich Schmidt <admin@deruli.de>
2014-11-03 22:48:21 +0100

    primary key und auto increment

--- sources/kundenbereich-1.0/src/post-install.php
+++ sources/kundenbereich-1.0/src/post-install.php
@@ -1,13 +1,11 @@
 <?php
 $create_shared_files_table = "CREATE TABLE IF NOT EXISTS `".tbname("shared_files")."` (
-`id` int(11) NOT NULL,
+`id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `filename` varchar(255) NOT NULL,
   `content` longtext NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
 
 db_query($create_shared_files_table);
-
-$add_primary_key = "ALTER TABLE `".tbname("shared_files")."` ADD PRIMARY KEY (`id`);";
- db_query($add_primary_key);
