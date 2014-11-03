0ce742b225ca5a1566aaec2daa2eb20b31aa3a77
Ulrich Schmidt <admin@deruli.de>
2014-11-03 20:50:37 +0100

    Upload Formular gebaut

--- sources/kundenbereich-1.0/src/modules/kundenbereich/kundenbereich_admin.php
+++ sources/kundenbereich-1.0/src/modules/kundenbereich/kundenbereich_admin.php
@@ -3,6 +3,34 @@ define("MODULE_ADMIN_REQUIRED_PERMISSION", "kundenbereich_upload");
 define("MODULE_ADMIN_HEADLINE", "Kundenbereich");
 
 
+function new_file($uid){
+echo "<form action=\"index.php?action=module_settings&module=kundenbereich\" method=\"post\" enctype=\"multipart/form-data\">";
+    $users = getUsers();
+     echo "<p>Kunde<br/>";
+     echo "<select name='user'>";
+     for($i = 0; $i < count($users); $i++){
+         $data = getUserByName($users[$i]);
+          echo "<option value=\"".$data["id"]."\">".$data["username"]."</option>";
+         }
+         
+         echo "</select>";
+         
+         echo "</p>";
+         echo "<p>Titel<br/>";
+         echo '<input type="text" style=\"width:300px;\" name="titel" value="">';
+         echo "</p>";
+         
+         echo "<p>Datei</br/>";
+         echo "<input type=\"file\" name=\"file\">";
+         echo "</p>";
+         
+         echo "<br/><p>";
+         echo "<input type=\"submit\" value=\"Hochladen\">";
+         echo "</p>";
+         
+         echo "</form>";
+}
+
 function list_files($uid){
 if(isset($_REQUEST["delete"])){
    db_query("DELETE FROM `".tbname("shared_files")."` WHERE id=".intval($delete));
@@ -49,9 +77,13 @@ echo "<ol>";
 }
 
 function kundenbereich_admin(){
-
 if(isset($_REQUEST["user"])){
+if(isset($_REQUEST["new"])){
+  new_file($_REQUEST["user"]);
+} else {
    list_files($_REQUEST["user"]);
+}
+
 } else{
 
   customer_list();
