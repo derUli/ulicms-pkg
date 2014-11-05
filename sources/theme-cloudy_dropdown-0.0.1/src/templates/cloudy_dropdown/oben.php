ee64fa88787eab6f9a52315ad52d6840944dfd80
Ulrich Schmidt <u.schmidt@velian.de>
2014-11-05 16:05:52 +0100

    files added

---
+++ sources/theme-cloudy_dropdown-0.0.1/src/templates/cloudy_dropdown/oben.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="<?php echo getCurrentLanguage();
+?>">
+<head>
+<?php base_metas()?>
+<link rel="stylesheet" media="screen" type="text/css" href="<?php echo getTemplateDirPath("cloudy_dropdown");
+?>style.css"/>
+</head>
+<body>
