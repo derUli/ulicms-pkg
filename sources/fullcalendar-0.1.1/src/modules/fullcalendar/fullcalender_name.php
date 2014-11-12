21efdbe1d76c1ce086b1e5262dbf68acc1e6b974
Ulrich Schmidt <admin@deruli.de>
2014-11-12 19:58:30 +0100

    Namen

---
+++ sources/fullcalendar-0.1.1/src/modules/fullcalendar/fullcalender_name.php
@@ -0,0 +1,12 @@
+<?php 
+function fullcalendar_name(){
+   $names = array();
+   $names["de"] = "Kalender";
+   $names["en"] = "Kalender";
+   
+   if(isset($names[$_SESSION["system_language"]])){
+      return $names[$_SESSION["system_language"]];
+   } else {
+      return $names["de"];
+   }
+}
