9c8017ad615e1ea42d762872009d8663720c01e2
Ulrich Schmidt <admin@deruli.de>
2014-11-19 19:09:28 +0100

    typo

--- sources/fullcalendar-0.1.1/src/modules/fullcalendar/fullcalendar_name.php
+++ sources/fullcalendar-0.1.1/src/modules/fullcalendar/fullcalendar_name.php
@@ -2,7 +2,7 @@
 function fullcalendar_name(){
    $names = array();
    $names["de"] = "Kalender";
-   $names["en"] = "Kalender";
+   $names["en"] = "Calendar";
    
    if(isset($names[$_SESSION["system_language"]])){
       return $names[$_SESSION["system_language"]];
