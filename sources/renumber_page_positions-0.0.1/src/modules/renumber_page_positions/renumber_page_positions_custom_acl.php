9a9e51a849310f3f34b00b3182e25405263ce94f
Ulrich Schmidt <admin@deruli.de>
2014-12-03 17:54:59 +0100

    Renumber Page Positions

---
+++ sources/renumber_page_positions-0.0.1/src/modules/renumber_page_positions/renumber_page_positions_custom_acl.php
@@ -0,0 +1,3 @@
+<?php
+global $acl_array;
+$acl_array["renumber_page_positions"] = null;
---
+++ sources/renumber_page_positions-0.0.1/src/modules/renumber_page_positions/renumber_page_positions_name.php
@@ -0,0 +1,12 @@
+<?php
+function renumber_page_positions_name(){
+     $names = array();
+     $names["de"] = "Seitenpositionen neu organisieren";
+     $names["en"] = "Reorganize Page Positions";
+    
+     if(isset($names[$_SESSION["system_language"]])){
+         return $names[$_SESSION["system_language"]];
+         }else{
+         return $names["de"];
+         }
+    }
