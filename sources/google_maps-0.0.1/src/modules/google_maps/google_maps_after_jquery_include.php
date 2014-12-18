7d9c6d24f18ada51e43d56d066140d201f7fd7f5
Ulrich Schmidt <admin@deruli.de>
2014-12-18 19:32:49 +0100

    Ready statt Read, lizenz

--- sources/google_maps-0.0.1/src/modules/google_maps/google_maps_after_jquery_include.php
+++ sources/google_maps-0.0.1/src/modules/google_maps/google_maps_after_jquery_include.php
@@ -1,34 +1,19 @@
 <?php
 
 if(containsModule(get_requested_pagename(), "google_maps")){
-
-
-
 $google_maps_marker = getconfig("google_maps_marker");
+$google_maps_marker = htmlspecialchars($google_maps_marker, ENT_QUOTES, "UTF-8");
 
-
-
-   $google_maps_marker = htmlspecialchars($google_maps_marker, ENT_QUOTES, "UTF-8");
-
-   
-
-   $google_zoom_level = getconfig("google_zoom_level");
-
-   
-
+$google_zoom_level = getconfig("google_zoom_level");
    if($google_zoom_level === false or $google_zoom_level == 0)
-
-      $google_zoom_level = 10;
-
-      
-
+     $google_zoom_level = 10;
 if($google_maps_marker and !empty($google_maps_marker)){
 
      ?>
 
      
 
-    <script src="http://maps.googleapis.com/maps/api/js?sensor=false" type="text/javascript"></script>
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
 
 <script type="text/javascript" src="<?php echo getModulePath("google_maps"); ?>gmap3.min.js"></script>
 
@@ -39,45 +24,23 @@ if($google_maps_marker and !empty($google_maps_marker)){
 
 
  <script type="text/javascript">
-
-      $(document).read(function(){
-
+      $(document).ready(function(){
         $('#gmap3').gmap3({
-
           marker:{
-
             address: "<?php echo $google_maps_marker;?>"
-
           },
-
           map:{
-
             options:{
-
               zoom: <?php echo intval($google_zoom_level);?>
-
             }
-
           }
-
         });
 
       });
 
     </script>
-
-
-
- 
-
-
-
 <?php
-
-
-
 }
-
      }
 
 ?>
