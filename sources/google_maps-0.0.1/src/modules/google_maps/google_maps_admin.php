58524d4a905b39b06b28999c292033c6fbfa6325
Ulrich Schmidt <u.schmidt@velian.de>
2014-12-19 10:16:24 +0100

    jquery_migrate und google_maps

---
+++ sources/google_maps-0.0.1/src/modules/google_maps/google_maps_admin.php
@@ -0,0 +1,41 @@
+<?php
+define("MODULE_ADMIN_HEADLINE", "Google Maps");
+define("MODULE_ADMIN_REQUIRED_PERMISSION", "google_maps");
+
+function google_maps_admin(){
+    
+     if(isset($_POST["submit"])){
+         setconfig("google_maps_marker",
+             db_escape($_POST["google_maps_marker"]));
+             
+         setconfig("google_maps_zoom_level", intval($_POST["google_maps_zoom_level"]));
+         }
+     
+     $google_maps_marker = getconfig("google_maps_marker");
+     
+     $google_maps_zoom_level = getconfig("google_maps_zoom_level");
+     $google_maps_zoom_level = intval($google_maps_zoom_level);
+    
+     ?>
+
+<form action="<?php echo getModuleAdminSelfPath()?>" method="post">
+<p><strong>Adresse, Ort</strong>
+<br/>
+<input type="text" name="google_maps_marker" value="<?php echo htmlspecialchars($google_maps_marker);?>">
+</p>
+
+<p><strong>Zoom Level</strong>
+<br/>
+<select name="google_maps_zoom_level">
+<?php for($i=0; $i <= 21; $i++){
+?>
+<option value="<?php echo $i;?>" <?php if($i == $google_maps_zoom_level) echo "selected";?> size= "1"><?php echo $i;?></option>
+<?php } ?>
+</select>
+
+<p><input type="submit" name="submit" value="Einstellungen speichern"/></p>
+</form>
+<?php
+     }
+
+?>
---
+++ sources/jquery_migrate-1.2.1/src/modules/jquery_migrate/jquery_migrate_after_jquery_include.php
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="<?php echo getModulePath("jquery_migrate");?>jquery-migrate-1.2.1.min.js"></script>
