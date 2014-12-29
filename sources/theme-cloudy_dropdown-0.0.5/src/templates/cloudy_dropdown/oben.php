cd5365ef5b5b5c078cf787d60f9329fbdc1b3312
Ulrich Schmidt <admin@deruli.de>
2014-12-30 00:53:44 +0100

    cloudy dropdown menue nun touch kompatibel7

--- sources/theme-cloudy_dropdown-0.0.5/src/templates/cloudy_dropdown/oben.php
+++ sources/theme-cloudy_dropdown-0.0.5/src/templates/cloudy_dropdown/oben.php
@@ -10,7 +10,12 @@
 
 <link rel="stylesheet" href="<?php echo getTemplateDirPath("cloudy_dropdown");
 ?>mobile.css" type="text/css" media="handheld"/>
-
+<script type="text/javascript" src="<?php echo getTemplateDirPath("cloudy_dropdown");?>doubletaptogo.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+   $(".menu_top li:has(ul)").doubleTapToGo();
+})
+</script>
 </head>
 <body>
 	<div class="root-container">
