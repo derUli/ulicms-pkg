58b2f8c5c3badab07095d158ebfb233d46d5016e
Ulrich Schmidt <admin@deruli.de>
2014-12-29 17:59:52 +0100

    create-faq

--- sources/faq-0.0.1/src/modules/faq/faq_admin.php
+++ sources/faq-0.0.1/src/modules/faq/faq_admin.php
@@ -10,10 +10,48 @@ if(!$action){
    $action = "list";
 }
 
+if($action == "delete"){
+$delete = intval($_GET["delete"]);
+
+db_query("DELETE FROM " . tbname("faq"). " WHERE id = ".$delete);
+
+$action = "list";
+}
+
+if(isset($_POST["create-faq"])){
+   $question = db_escape($_POST["question"]);
+   $answer = db_escape($_POST["answer"]);
+   
+   db_query("INSERT INTO ". tbname("faq"). " (question, answer)
+   VALUES('$question', '$answer')");
+}
+
+if($action == "new"){
+?>
+<form action="action=module_settings&module=faq&action=list" method="post">
+<table>
+<tr>
+<td>Frage</td>
+<td><input type="text" name="question" value="Warum ist die Banane Krum?"></td>
+</tr>
+<tr>
+<td>Antwort</td>
+<td><textarea name="answer" cols="50" rows="10">Weil niemand in den Urwald zog, und die Banane gerade bog.</textarea>></td>
+</tr>
+<tr>
+<td>
+</td>
+<td><input type="submit" name="create-faq" value="create-faq">
+</td>
+</tr>
+</form>
+<?
+}
 if($action == "list"){
 
 ?>
-
+<p><strong><a href="?action=module_settings&module=faq&action=new">[Frage erstellen]/a></strong>
+</p>
 <?php
 $sql = db_query("SELECT * FROM ".tbname("faq"). " ORDER by question ASC");
 if(db_num_rows($sql) > 0){
