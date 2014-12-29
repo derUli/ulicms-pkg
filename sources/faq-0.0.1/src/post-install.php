325c083dbc68706b1bd0a749691a0050be5e9edc
Ulrich Schmidt <admin@deruli.de>
2014-12-29 18:11:09 +0100

    typo

--- sources/faq-0.0.1/src/post-install.php
+++ sources/faq-0.0.1/src/post-install.php
@@ -6,7 +6,7 @@ question VARCHAR(255) NULL,
 answer text NULL
 )");
 
-$q = query("select id from ".tbname("faq"). " LIMIT 1");
+$q = db_query("select id from ".tbname("faq"). " LIMIT 1");
 if(db_num_rows($q) == 0){
   $question = db_escape("Was ist UliCMS?");
   $answer = db_escape("UliCMS ist ein professionelles Web Content Management-System welches von Ulrich Schmidt seit dem Jahr 2011 entwickelt wird.");
