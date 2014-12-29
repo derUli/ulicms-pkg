62a2e5a3d4357b49716d2bd24e71e4dc9e970f4a
Ulrich Schmidt <admin@deruli.de>
2014-12-29 18:35:00 +0100

    typos fixed

--- sources/faq-0.0.1/src/modules/faq/faq_admin.php
+++ sources/faq-0.0.1/src/modules/faq/faq_admin.php
@@ -4,6 +4,19 @@ define("MODULE_ADMIN_REQUIRED_PERMISSION", "faq_edit");
 
 function faq_admin(){
 
+?>
+<style type="text/css">
+
+table th{
+text-align:left;
+}
+
+input[type="text"], textarea{
+width:300px;
+}
+</style>
+<?php
+
 $action = $_GET["do"];
 
 if(!$action){
@@ -39,20 +52,21 @@ if($action == "new"){
 <form action="action=module_settings&module=faq&do=list" method="post">
 <table>
 <tr>
-<td>Frage</td>
+<td style="width:150px">Frage</td>
 <td><input type="text" name="question" size="50" maxlength="255" value="Warum ist die Banane Krum?"></td>
 </tr>
 <tr>
-<td>Antwort</td>
-<td><textarea name="answer" cols="50" rows="10">Weil niemand in den Urwald zog, und die Banane gerade bog.</textarea>></td>
+<td style="width:150px">Antwort</td>
+<td><textarea name="answer" cols="50" rows="10">Weil niemand in den Urwald zog, und die Banane gerade bog.</textarea></td>
 </tr>
 <tr>
 <td>
 </td>
-<td><input type="hidden" name="create-faq" value="create-faq">
-<input type="submit" value="Frage Erstellen">
+<td style="text-align:center"><input type="hidden" name="create-faq" value="create-faq">
+<p><input type="submit" value="Frage Erstellen"></p>
 </td>
 </tr>
+</table>
 </form>
 <?
 }
@@ -65,21 +79,22 @@ $data = db_fetch_object($sql);
 <form action="action=module_settings&module=faq&do=list" method="post">
 <table>
 <tr>
-<td>Frage</td>
+<td style="width:150px">Frage</td>
 <td><input type="text" size="50" maxlength="255" name="question" value="<?php echo htmspecialchars($data->question, ENT_QUOTES, "utf-8");?>"></td>
 </tr>
 <tr>
-<td>Antwort</td>
+<td style="width:150px">Antwort</td>
 <td><textarea name="answer" cols="50" rows="10"><?php echo nl2br(htmspecialchars($data->answer));?></textarea>></td>
 </tr>
 <tr>
 <td>
 </td>
-<td><input type="hidden" name="edit-faq" value="<?php echo $data->id;?>">
-
-<input type="submit" value="Frage Speichern">
+<td style="text-align:center"><input type="hidden" name="edit-faq" value="<?php echo $data->id;?>">
+<p>
+<input type="submit" value="Frage Speichern"></p>
 </td>
 </tr>
+</table>
 </form>
 
 <?php
@@ -99,16 +114,16 @@ if(db_num_rows($sql) > 0){
 <tr>
 <th>Frage</th>
 <th>Antwort</th>
-<th>Bearbeiten</th>
-<th>Löschen</th>
+<th></th>
+<th></th>
 </tr>
 <?php while($row = db_fetch_object($sql)){
 echo '<tr>';
 echo '<td>'.htmlspecialchars($row->question).'</td>';
 echo '<td>'.nl2br(htmlspecialchars($row->answer), 60).'</td>';
-echo '<td>'.'<a href="?action=module_settings&module=faq&do=edit&edit='.$row->id.'"><img src="gfx/edit.gif" alt="Bearbeiten" title="Bearbeiten"></a>'.'</td>';
+echo '<td style="text-align:center">'.'<a href="?action=module_settings&module=faq&do=edit&edit='.$row->id.'"><img src="gfx/edit.gif" alt="Bearbeiten" title="Bearbeiten"></a>'.'</td>';
 
-echo '<td>'.'<a href="?action=module_settings&module=faq&do=delete&delete='.$row->id.'" onclick="return confirm(\'Wirklich löschen?\')"><img src="gfx/delete.gif" alt="Löschen" title="Löschen"></a>'.'</td>';
+echo '<td style="text-align:center">'.'<a href="?action=module_settings&module=faq&do=delete&delete='.$row->id.'" onclick="return confirm(\'Wirklich löschen?\')"><img src="gfx/delete.gif" alt="Löschen" title="Löschen"></a>'.'</td>';
 echo '</tr>';
 }?>
 </table>
