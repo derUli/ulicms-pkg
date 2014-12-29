b58948d4ce4b4c06a9fb7fd3273ecb3adac202dc
Ulrich Schmidt <admin@deruli.de>
2014-12-29 18:12:12 +0100

    typo und encoding

--- sources/faq-0.0.1/src/modules/faq/faq_admin.php
+++ sources/faq-0.0.1/src/modules/faq/faq_admin.php
@@ -86,7 +86,7 @@ $data = db_fetch_object($sql);
 if($action == "list"){
 
 ?>
-<p><strong><a href="?action=module_settings&module=faq&action=new">[Frage erstellen]/a></strong>
+<p><strong><a href="?action=module_settings&module=faq&action=new">[Frage erstellen]</a></strong>
 </p>
 <?php
 $sql = db_query("SELECT * FROM ".tbname("faq"). " ORDER by question ASC");
@@ -97,7 +97,7 @@ if(db_num_rows($sql) > 0){
 <td>Frage</td>
 <td>Antwort</td>
 <td>Bearbeiten</td>
-<td>Löschen</td>
+<td>LÃ¶schen</td>
 </th>
 <?php while($row = db_fetch_object($sql)){
 echo '<tr>';
@@ -105,7 +105,7 @@ echo '<td>'.htmlspecialchars($row->question).'</td>';
 echo '<td>'.nl2br(htmlspecialchars($row->answer)).'</td>';
 echo '<td>'.'<a href="?action=module_settings&module=faq&action=edit&edit='.$row->id.'"><img src="gfx/edit.gif" alt="Bearbeiten" title="Bearbeiten"></a>'.'</td>';
 
-echo '<td>'.'<a href="?action=module_settings&module=faq&action=delete&delete='.$row->id.'" onclick="return confirm(\'Wirklich löschen?\')"><img src="gfx/delete.gif" alt="Löschen" title="Löschen"></a>'.'</td>';
+echo '<td>'.'<a href="?action=module_settings&module=faq&action=delete&delete='.$row->id.'" onclick="return confirm(\'Wirklich lÃ¶schen?\')"><img src="gfx/delete.gif" alt="LÃ¶schen" title="LÃ¶schen"></a>'.'</td>';
 echo '</tr>';
 }?>
 </table>
