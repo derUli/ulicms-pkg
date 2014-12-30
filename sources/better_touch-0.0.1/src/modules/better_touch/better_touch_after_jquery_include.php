6cf62d4d6c767dc6dd8816d2c033dd6f236da833
Ulrich Schmidt <admin@deruli.de>
2014-12-30 01:21:11 +0100

    better touch auch fuer Frontend Menues

---
+++ sources/better_touch-0.0.1/src/modules/better_touch/better_touch_after_jquery_include.php
@@ -0,0 +1,9 @@
+<script type="text/javascript" src="<?php echo getModulePath("better_touch");?>doubletaptogo.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+$(".menu_top li:has(ul)").doubleTapToGo();
+$(".menu_bottom li:has(ul)").doubleTapToGo();
+$(".menu_left li:has(ul)").doubleTapToGo();
+$(".menu_right li:has(ul)").doubleTapToGo();
+})
+</script>
