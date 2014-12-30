c6731af835ef029f7ea00d7e50a3080ebccb9997
Ulrich Schmidt <admin@deruli.de>
2014-12-30 01:17:18 +0100

    better touch

---
+++ sources/better_touch-0.0.1/src/modules/better_touch/better_touch_admin_head.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="<?php echo getModulePath("better_touch");?>doubletaptogo.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+$(".navbar_top ul li:has(ul)").doubleTapToGo();
+})
+</script>
