0221377f7d49479d26403349e0430cd508395565
Ulrich Schmidt <ulrich1992@web.de>
2015-01-03 22:28:36 +0100

    fix

--- sources/collapse_submenus-0.0.1/src/modules/collapse_submenus/collapse_submenus_after_jquery_include.php
+++ sources/collapse_submenus-0.0.1/src/modules/collapse_submenus/collapse_submenus_after_jquery_include.php
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $("ul.menu_left li:has(ul) li").hide(); 
   $("ul.menu_right li:has(ul) li").hide(); 
   $("a.menu_active_link").parents().show();
-  $("a.menu_active_link").parent().find(".sub_menu li").show();
+  $("a.menu_active_link").parents().children().first().show();
 });
 </script>
+
