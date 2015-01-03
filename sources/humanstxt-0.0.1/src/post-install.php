35095c98d9c88375cc6c5fde289ca2ca325d1ef0
Ulrich Schmidt <ulrich1992@web.de>
2015-01-03 14:12:41 +0100

    humans.txt hinzugefügt

---
+++ sources/humanstxt-0.0.1/src/post-install.php
@@ -0,0 +1,12 @@
+<?php
+$template = file_get_contents(getModulePath("humanstxt"). "/template.txt");
+
+$file = ULICMS_ROOT. "/humans.txt";
+if(!file_exists($file)){
+  echo "Erstelle humans.txt Vorlage";
+  file_put_contents($file, $template);
+  echo "Fertig";
+}
+else {
+   echo "humans.txt existiert bereits";
+}
