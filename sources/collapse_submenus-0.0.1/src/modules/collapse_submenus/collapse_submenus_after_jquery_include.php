b3b6b291ff6f4f1ae56b5b1c6fe65b4ad7d87e8a
Ulrich Schmidt <ulrich1992@web.de>
2015-01-03 22:44:19 +0100

    typo fixed

--- sources/collapse_submenus-0.0.1/src/modules/collapse_submenus/collapse_submenus_after_jquery_include.php
+++ sources/collapse_submenus-0.0.1/src/modules/collapse_submenus/collapse_submenus_after_jquery_include.php
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $("ul.menu_left li:has(ul) li").hide(); 
   $("ul.menu_right li:has(ul) li").hide(); 
   $("a.menu_active_link").parents().show();
-  $("a.menu_active_link").parents().children().show();
+  $("a.menu_active_link").parents().children("li").show();
 });
 </script>
 
