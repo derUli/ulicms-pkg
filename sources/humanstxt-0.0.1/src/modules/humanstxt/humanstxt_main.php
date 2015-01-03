f66539ba45628a592c692f57c242c6efa94ee17f
Ulrich Schmidt <ulrich1992@web.de>
2015-01-03 14:08:36 +0100

    Typo fixed

--- sources/humanstxt-0.0.1/src/modules/humanstxt/humanstxt_main.php
+++ sources/humanstxt-0.0.1/src/modules/humanstxt/humanstxt_main.php
@@ -2,7 +2,7 @@
 function humanstxt_render(){
    $file = ULICMS_ROOT. "/humans.txt";
    $html = "humans.txt existiert nicht";
-   if(file_exists($file){
+   if(file_exists($file)){
        $html = nl2br(htmlspecialchars(file_get_contents($file))) ;
     }
     return $html;
