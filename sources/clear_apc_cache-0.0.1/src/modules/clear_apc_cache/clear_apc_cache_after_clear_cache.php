606fb7931e5c018b8919ee903cffabbd43424010
Ulrich Schmidt <u.schmidt@velian.de>
2015-01-08 16:14:43 +0100

    clear_apc_cache hinzugefügt

---
+++ sources/clear_apc_cache-0.0.1/src/modules/clear_apc_cache/clear_apc_cache_after_clear_cache.php
@@ -0,0 +1,4 @@
+<?php
+$include_file = getModulePath("clear_apc_cache"). "/clear_apc_cache_main.php";
+include_once $include_file;
+clear_apc_cache_main();
---
+++ sources/clear_apc_cache-0.0.1/src/modules/clear_apc_cache/clear_apc_cache_main.php
@@ -0,0 +1,10 @@
+<?php
+function clear_apc_cache_render(){
+     if(!function_exists("apc_clear_cache")){
+       return "<p style='color:red'>Der APC Cache kann nicht gelernt werden, da APC nicht installiert ist.</p>";
+  }
+  apc_clear_cache();
+  apc_clear_cache('user');
+  apc_clear_cache('opcode');
+  return "<p style='color:green'>Der APC Cache wurde erfolgreich geleert.</p>";"
+}
