7bc6c65d300148c756de3bca4d1ca41cfa80fc0c
Ulrich Schmidt <admin@deruli.de>
2015-01-10 15:40:38 +0100

    https

---
+++ sources/ip_check-0.0.1/src/modules/ip_check/ip_check_main.php
@@ -0,0 +1,6 @@
+<?php
+function ip_check_render(){
+   return '<a href="https://ip-check.info/?lang=en" title="IP check">
+   <img src="https://ip-check.org/ip-check.png?edgewise=true" alt="IP check" width="150" height="150">
+</a>';
+}
