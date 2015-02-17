055b6373bbcd90ce03338e38fbc2cd7cff70dd05
Ulrich Schmidt <admin@deruli.de>
2015-02-17 23:15:27 +0100

    IP-Check 002

---
+++ sources/ip_check-0.0.2/src/modules/ip_check/ip_check_main.php
@@ -0,0 +1,6 @@
+<?php
+function ip_check_render(){
+   return '<a href="https://ip-check.info/?lang=en" title="IP check">
+   <img src="https://ip-check.org/ip-check.png?edgewise=true&lang='.htmlspecialchars($_SESSION["language"]).' alt="IP check" width="150" height="150">
+</a>';
+}
