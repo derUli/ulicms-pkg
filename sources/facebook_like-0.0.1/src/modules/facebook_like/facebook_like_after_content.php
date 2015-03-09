faf8d41f424e9ae780bdb244fd219b558b6fa220
Ulrich Schmidt <admin@deruli.de>
2015-03-09 19:28:11 +0100

    options

--- sources/facebook_like-0.0.1/src/modules/facebook_like/facebook_like_after_content.php
+++ sources/facebook_like-0.0.1/src/modules/facebook_like/facebook_like_after_content.php
@@ -1,6 +1,10 @@
 <?php
+$facebook_like_button_layout = get config("facebook_like_button_layout");
+$facebook_like_button_data_colorscheme = getconfig("facebook_like_button_data_colorscheme");
+$facebook_like_button_action = getconfig("facebook_like_button_action");
+$facebook_like_button_show_faces = getconfig("facebook_like_button_show_faces");
 $data = get_custom_data();
 if(!isset($data["disable_facebook_like"])){
 ?>
-<div class="fb-like" data-href="<?php echo htmlspecialchars(getCurrentURL());?>" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+<div class="fb-like" data-href="<?php echo htmlspecialchars(getCurrentURL());?>" data-layout="<?php echo $facebook_like_button_layout;?>" data-colorscheme="<?php echo $facebook_like_button_data_colorscheme;?>" data-action="<?php echo $facebook_like_button_action;?>" data-show-faces="<?php echo $facebook_like_button_show_faces;?>" data-share="<?php echo $facebook_like_button_share;?>"></div>
 <?php } ?>
---
+++ sources/facebook_like-0.0.1/src/post-install.php
@@ -0,0 +1,28 @@
+<?php
+if(!getconfig("facebook_like_button_layout"))
+   setconfig("facebook_like_button_layout", "standard");
+   
+if(!getconfig("facebook_like_button_action"))
+   setconfig("facebook_like_button_action", "like");
+   
+if(!getconfig("facebook_like_button_data_colorscheme"))
+   setconfig("facebook_like_button_data_colorscheme", "light");
+   
+if(getconfig("facebook_like_button_show_faces") === false)
+   setconfig("facebook_like_button_show_faces", "true");
+   
+if(getconfig("facebook_like_button_shware") === false)
+   setconfig("facebook_like_button_share", "true");
+   
+?>
+<pre>
+Sie können optional die folgenden Konfigurationseinstellungen 
+anpassen.
+facebook_like_button_layout
+facebook_like_button_action
+facebook_like_button_data_colorscheme
+facebook_like_button_show_faces
+facebook_like_button_show_faces
+facebook_like_button_share
+Mögliche Werte können Sie sehen im Facebook Developer Netzwerk
+</pre>
