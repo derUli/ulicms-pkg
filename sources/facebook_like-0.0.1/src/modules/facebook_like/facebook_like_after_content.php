10f26cfd9a78734bed1fd0487fce949da5fc07cb
Ulrich Schmidt <u.schmidt@velian.de>
2015-03-09 16:15:23 +0100

    disable_facebook_like

--- sources/facebook_like-0.0.1/src/modules/facebook_like/facebook_like_after_content.php
+++ sources/facebook_like-0.0.1/src/modules/facebook_like/facebook_like_after_content.php
@@ -1,1 +1,6 @@
+<?php
+$data = get_custom_data();
+if(!isset($data["disable_facebook_like"])){
+?>
 <div class="fb-like" data-href="<?php echo htmlspecialchars(getCurrentURL());?>" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+<?php } ?>
