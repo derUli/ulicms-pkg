73592ee79a00162040336b4be7cb00a8c1ec1a97
Ulrich Schmidt <admin@deruli.de>
2015-03-09 19:32:46 +0100

    fiux

---
+++ sources/facebook_sdk-2.0/src/post-install.php
@@ -0,0 +1,3 @@
+<pre>Sie müssen die Konfigurationseinstellung facebook_app_id nun setzen,
+sofern Sie das noch nicht getan haben.
+Eine Facebook APP-ID können Sie <a href="https://developers.facebook.com/docs/plugins/like-button">hier erstellen</a></pre>
