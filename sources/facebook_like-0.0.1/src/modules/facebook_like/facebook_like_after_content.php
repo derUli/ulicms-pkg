8e11be96f82bf8016a15803b7327932acaaea1d6
Ulrich Schmidt <admin@deruli.de>
2015-03-09 19:37:23 +0100

    semikolon

--- sources/facebook_like-0.0.1/src/modules/facebook_like/facebook_like_after_content.php
+++ sources/facebook_like-0.0.1/src/modules/facebook_like/facebook_like_after_content.php
@@ -3,7 +3,7 @@ $facebook_like_button_layout = get config("facebook_like_button_layout");
 $facebook_like_button_data_colorscheme = getconfig("facebook_like_button_data_colorscheme");
 $facebook_like_button_action = getconfig("facebook_like_button_action");
 $facebook_like_button_show_faces = getconfig("facebook_like_button_show_faces");
-$facebook_like_button_share = getconfig("facebook_like_button_share")
+$facebook_like_button_share = getconfig("facebook_like_button_share");
 $data = get_custom_data();
 if(!isset($data["disable_facebook_like"])){
 ?>
