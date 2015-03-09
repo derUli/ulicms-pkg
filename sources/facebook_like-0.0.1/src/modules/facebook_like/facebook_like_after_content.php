236d3a3e302736b16d35717b7b28e43363861841
Ulrich Schmidt <admin@deruli.de>
2015-03-09 19:38:09 +0100

    typo

--- sources/facebook_like-0.0.1/src/modules/facebook_like/facebook_like_after_content.php
+++ sources/facebook_like-0.0.1/src/modules/facebook_like/facebook_like_after_content.php
@@ -1,5 +1,5 @@
 <?php
-$facebook_like_button_layout = get config("facebook_like_button_layout");
+$facebook_like_button_layout = getconfig("facebook_like_button_layout");
 $facebook_like_button_data_colorscheme = getconfig("facebook_like_button_data_colorscheme");
 $facebook_like_button_action = getconfig("facebook_like_button_action");
 $facebook_like_button_show_faces = getconfig("facebook_like_button_show_faces");
