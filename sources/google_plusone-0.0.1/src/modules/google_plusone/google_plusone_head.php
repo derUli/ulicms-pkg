bbf4fb0f04e75514ef29ea94ff52b33dcf74d727
Ulrich Schmidt <admin@deruli.de>
2015-03-18 17:44:39 +0100

    typo

--- sources/google_plusone-0.0.1/src/modules/google_plusone/google_plusone_head.php
+++ sources/google_plusone-0.0.1/src/modules/google_plusone/google_plusone_head.php
@@ -1,2 +1,6 @@
-
-<script type="text/javascript" src="https://apis.google.com/js/plusone.js">/script>
+<?php
+$data = get_custom_data();
+if(!isset($data["disable_facebook_like"])){
+?>
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+<?php }
