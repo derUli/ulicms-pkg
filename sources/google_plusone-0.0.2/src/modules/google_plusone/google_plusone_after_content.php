17fd7450a38100a123b236b4b455d8ac7bf07dd8
Ulrich Schmidt <u.schmidt@velian.de>
2015-03-19 13:37:38 +0100

    social media gedöns nur bei http 200 anzeigen

--- sources/google_plusone-0.0.2/src/modules/google_plusone/google_plusone_after_content.php
+++ sources/google_plusone-0.0.2/src/modules/google_plusone/google_plusone_after_content.php
@@ -1,6 +1,6 @@
 <?php
 $data = get_custom_data();
-if(!isset($data["disable_google_plusone"])){
+if(!isset($data["disable_google_plusone"]) and is_200()){
 ?>
 <span class="plusone">
 <g:plusone></g:plusone>
