91b80425f0663b76d7bc2366f1322ad0494f4965
Ulrich Schmidt <ulrich1992@web.de>
2015-04-24 23:05:05 +0200

    breadcrumbs-0.0.1 hinzugefügt

---
+++ sources/breadcrumbs-0.0.1/src/modules/breadcrumbs/breadcrumbs_after_init.php
@@ -0,0 +1,33 @@
+<?php
+if(!function_exists("breadcrumbs")){
+  function get_breadcrumbs(){
+    if(!is_200())
+       return "";
+    $page = get_page();
+    $parent = $page["parent"];
+    $parent_name = getPageSystemnameByID($parent);
+    $html = "<a href=\"".buildSEOUrl($page["systemname"], $page["redirection"])."\">".htmlspecialchars($page["title"]). "</a>";
+    while($parent != null){
+
+      $page = get_page($parent_name);
+      $parent = $page["parent"];
+      $parent_name = getPageSystemnameByID($parent);
+      $html = "<a href=\"".buildSEOUrl($page["systemname"], $page["redirection"])."\">".htmlspecialchars($page["title"]). "</a>". " &gt; ".$html;
+}
+
+    $html = "<a href=\"".buildSEOUrl(get_frontpage(), $page["redirection"])."\">".get_translation("frontpage"). "</a>". " &gt; ".  $html;
+
+    $html = '<div class="breadcrumb_nav">'.$html."</div>";
+   return $html;
+}
+
+
+}
+
+
+if(!function_exists("breadcrumbs")){
+function breadcrumbs(){
+  echo get_breadcrumbs();
+}
+
+}
