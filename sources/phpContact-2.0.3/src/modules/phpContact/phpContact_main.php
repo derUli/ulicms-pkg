c8f9d3e58d67c1f8f462d62d429145f5822582e0
Ulrich Schmidt <u.schmidt@velian.de>
2015-04-27 15:36:34 +0200

    phpContact-2.0.3 hinzugefügt

---
+++ sources/phpContact-2.0.3/src/modules/phpContact/phpContact_main.php
@@ -0,0 +1,14 @@
+<?php
+function phpContact_render(){
+   // disable Anti CSRF Token check, since we are not able to add such a token to the contact form
+   no_anti_csrf();
+   $file = ULICMS_ROOT."/phpContact/index.php";
+   $retval = "<span class=\"ulicms_error\">Bitte installieren Sie zuerst phpContact nach ".$file.".</span>";
+   if(is_file($file)){
+     ob_start();
+     include_once $file;
+     $retval = ob_get_clean();
+ 
+   }
+    return $retval;
+}
