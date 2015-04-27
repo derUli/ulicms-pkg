6d185f1397bae5f4e1359b034a281bb0906d63d6
Ulrich Schmidt <u.schmidt@velian.de>
2015-04-27 13:53:36 +0200

    phpContact Integartion

---
+++ sources/phpContact-2.0.3/src/modules/phpContact/phpContact_head.php
@@ -0,0 +1,6 @@
+<?php 
+if(containsModule(null, "phpContact")){
+?>
+<link rel="stylesheet" href="phpContact/templates/Default/css/style.css" type="text/css" />
+
+<?php } ?>
---
+++ sources/phpContact-2.0.3/src/modules/phpContact/phpContact_main.php
@@ -0,0 +1,12 @@
+<?php
+function phpContact_render(){
+   $file = ULICMS_ROOT."/phpContact/index.php";
+   $retval = "<span class=\"ulicms_error\">Bitte installieren Sie zuerst phpContact nach ".$path.".</span>";
+   if(is_file($path)){
+     ob_start();
+     include_once $file;
+     $retval = ob_get_contents();
+ 
+   }
+    return $retval;
+}
