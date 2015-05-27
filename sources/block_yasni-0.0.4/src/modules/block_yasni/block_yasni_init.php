4f00bd104e30f56d5b9542284cf5cf3447bbd693
Ulrich Schmidt <admin@deruli.de>
2015-05-27 23:33:28 +0200

    host

--- sources/block_yasni-0.0.4/src/modules/block_yasni/block_yasni_init.php
+++ sources/block_yasni-0.0.4/src/modules/block_yasni/block_yasni_init.php
@@ -7,8 +7,8 @@ if($hostname){
      $list = array("123people.de", "findestars.de", "myonid.de",
          "peekyou.com", "pipl.com", "rapleaf.com", "snitch.name",
          "spock.com", "tweepz.com", "wink.com", "yasni.de",
-         "yoname.com", "yourtraces.com", "zoominfo.com", "umsonstleutesuchen.de",
-         "wer-ist.org");
+         "yoname.com", "yourtraces.com", "zoominfo.com", "adressendeutschland.de", "umsonstleutesuchen.de",
+         "wer-ist.org", "crawler-prod.it2media.de");
     
      for($s = 0; $s < count($list); $s++){
          if(endsWith($hostname, $list[$s])){
