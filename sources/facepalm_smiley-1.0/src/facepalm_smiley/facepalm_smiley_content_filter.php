8a78b21a89bb6e44cc622f3b440d3b42b9435d6a
Ulrich Schmidt <admin@deruli.de>
2015-05-30 17:07:31 +0200

    -alt

--- sources/facepalm_smiley-1.0/src/facepalm_smiley/facepalm_smiley_content_filter.php
+++ sources/facepalm_smiley-1.0/src/facepalm_smiley/facepalm_smiley_content_filter.php
@@ -2,6 +2,6 @@
 function facepalm_smiley_content_filter($html){
    $dir = getModulePath ("facepalm_smiley");
    $imgurl = $dir . '/img/facepalm.gif';
-   $itag = '<img src="' . $imgurl . '" alt="m(" class="wp-smiley" />';
+   $itag = '<img src="' . $imgurl . '" alt="m("/>';
    return preg_replace ('/[mM]\(/', $itag, $html);
 }
