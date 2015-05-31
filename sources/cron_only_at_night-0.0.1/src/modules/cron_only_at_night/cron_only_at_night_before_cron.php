c8a2311ef302ff35859bb89790cd88a49cf1fae8
Ulrich Schmidt <admin@deruli.de>
2015-05-31 16:53:30 +0200

    cronjob

--- sources/cron_only_at_night-0.0.1/src/modules/cron_only_at_night/cron_only_at_night_before_cron.php
+++ sources/cron_only_at_night-0.0.1/src/modules/cron_only_at_night/cron_only_at_night_before_cron.php
@@ -1,6 +1,6 @@
 <?php
 if(!function_exists("is_night")){
-// Check if it is night (current hour between 0 and 4 o'Clock AM)
+// Check if it is night (current hour between 0 and 4 O'Clock AM)
 function is_night(){
    $hour = date("G", time());
    return  $hour >= 0 and $hour <= 4;
