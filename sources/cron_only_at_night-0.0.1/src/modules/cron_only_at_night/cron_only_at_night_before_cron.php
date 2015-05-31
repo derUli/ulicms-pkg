3c1bc425126f22e5c34387a863d0975bd28a236a
Ulrich Schmidt <admin@deruli.de>
2015-05-31 17:04:29 +0200

    int

--- sources/cron_only_at_night-0.0.1/src/modules/cron_only_at_night/cron_only_at_night_before_cron.php
+++ sources/cron_only_at_night-0.0.1/src/modules/cron_only_at_night/cron_only_at_night_before_cron.php
@@ -2,12 +2,11 @@
 if(!function_exists("is_night")){
 // Check if it is night (current hour between 0 and 4 O'Clock AM)
 function is_night(){
-   $hour = date("G", time());
-   return  $hour >= 0 and $hour <= 4;
+   $hour = (int)date("G", time());
+   return $hour >= 0 and $hour <= 4;
 }
 
 }
-
 
 if(!is_night()){
    exit();
