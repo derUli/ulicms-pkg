4ea97f7fa996151186d47f20055d11e068db5f41
Ulrich Schmidt <admin@deruli.de>
2015-06-10 21:26:06 +0200

    search

--- sources/search-0.6.0/src/modules/search/search_lang_de.php
+++ sources/search-0.6.0/src/modules/search/search_lang_de.php
@@ -3,7 +3,7 @@ define("TRANSLATION_SEARCH_SETTINGS", "Suchfunktion Einstellungen");
 define("TRANSLATION_ENABLE_BOOLEAN_MODE", "Boolean Mode aktivieren");
 define("TRANSLATION_SEARCH_SUBJECT", "Suchbegriff:");
 define("TRANSLATION_DO_SEARCH", "Suchen");
-define("TRANSLATION_AREA", "Bereich");
+define("TRANSLATION_AREA", "Bereich:");
 define("TRANSLATION_BLOG", "Blog");
 define("TRANSLATION_COMMENTS", "Kommentare");
 define("TRANSLATION_EVENTS", "Veranstaltungen");
