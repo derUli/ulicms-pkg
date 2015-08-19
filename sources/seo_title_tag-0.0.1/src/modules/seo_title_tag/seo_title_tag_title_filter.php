f23e505391c6dbb018d4ce9fbfd954e71d8e55c0
Ulrich Schmidt <admin@deruli.de>
2015-08-19 18:47:45 +0200

    title

--- sources/seo_title_tag-0.0.1/src/modules/seo_title_tag/seo_title_tag_title_filter.php
+++ sources/seo_title_tag-0.0.1/src/modules/seo_title_tag/seo_title_tag_title_filter.php
@@ -3,11 +3,7 @@ function seo_title_tag_title_filter($title){
       $custom_data = get_custom_data();
       if(isset($custom_data["seo_title"]) and 
         !empty($custom_data["seo_title"])){
-          $title = str_ireplace("%seo_title%", $custom_data["seo_title"], $title);
-      } else {
-          $page = get_page();
-          $page_title = $page["title"];
-          $title = str_ireplace("%seo_title%", $page_title, $title);
+          $title = str_ireplace("%title%", $custom_data["seo_title"], $title);
       }
       
       return $title;
