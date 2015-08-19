a65523a37eee67d12a76402239bd1550307e9eda
Ulrich Schmidt <admin@deruli.de>
2015-08-19 18:38:25 +0200

    seo_title_tag

---
+++ sources/seo_title_tag-0.0.1/src/modules/custom_title_tag/seo_title_tag_main.php
@@ -0,0 +1,4 @@
+<?php
+function seo_title_tag_render(){
+     return "Kein Embed Modul";
+    }
---
+++ sources/seo_title_tag-0.0.1/src/modules/custom_title_tag/seo_title_tag_title_filter.php
@@ -0,0 +1,13 @@
+<?php
+function seo_title_tag_title_filter($title){
+      $custom_data = get_custom_data();
+      if(isset($custom_data["seo_title"]) and 
+        !empty($custom_data["seo_title"])){
+          $title = str_replace("%seo_title%", $custom_data["seo_title"], $title);
+      } else {
+      
+          $title = str_replace("%title_tag%", get_title(), $title);
+      }
+      
+      return $title;
+}
