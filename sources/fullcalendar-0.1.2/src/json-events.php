825b27d571205076054094cd874fde3d49af3f2c
Ulrich Schmidt <admin@deruli.de>
2015-12-03 22:24:44 +0100

    Fullcalendar

---
+++ sources/fullcalendar-0.1.2/src/json-events.php
@@ -0,0 +1,45 @@
+<?php
+ include "init.php";
+ include "templating.php";
+ $events = array();
+
+$c = getconfig("cache_type");
+switch($c){
+ case "cache_lite":
+     @include "classes/Cache/Lite.php";
+     $cache_type = "cache_lite";
+    
+     break;
+ case "file": default:
+     $cache_type = "file";
+     break;
+     break;
+     }
+
+header('Content-Type: application/json; charset=utf8');
+
+ if(!isset($_REQUEST["start"])){
+    $_REQUEST["start"] = mktime(0, 0, 0, date("m"), 1, date("y"));
+ }
+
+ if(!isset($_REQUEST["end"])){
+    $_REQUEST["end"] = mktime(0, 0, 0, date("m") + 1, 0, date("y"));
+}
+
+ $query = db_query("SELECT * FROM `" . tbname("events") . "` WHERE `start` > " . intval($_REQUEST["start"]) . " AND `end` <" . intval($_REQUEST["end"]) . " ORDER BY id");
+ while($row = db_fetch_object($query)){
+	 $obj = array();
+	 $obj["id"] = $row -> id;
+	 $obj["start"] = date("Y-m-d", $row -> start);
+	 $obj["end"] = date("Y-m-d", $row -> end);
+	 $obj["title"] = $row -> title;
+
+	 if(!empty($row -> url) and $row -> url != "http://")
+		 $obj["url"] = $row -> url;;
+
+	 array_push($events, $obj);
+
+ }
+
+ $json = json_encode($events);
+ die($json);
