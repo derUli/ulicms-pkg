cec2ffa1ef866e7364173306d5ca6c09b7a6e35b
Ulrich Schmidt <admin@deruli.de>
2016-02-01 22:24:53 +0100

    default

--- sources/last_updated-1.0/src/content/modules/last_updated/templates/default.php
+++ sources/last_updated-1.0/src/content/modules/last_updated/templates/default.php
@@ -1,7 +1,14 @@
 <?php
 $page = get_page ();
-if (isset ( $page ["lastmodified"] ) and $page ["lastmodified"] > 0) {
+if (!containsModule() and isset ( $page ["lastmodified"] ) and $page ["lastmodified"] > 0) {
+?>
+<div class="last-updated">
+<?php
 	translate ( "last_updated", array (
 			"%date%" => strftime ( "%X %x", $page ["lastmodified"] ) 
 	) );
+	
+	?>
+	</div>
+	<?php
 }
