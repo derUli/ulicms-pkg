d1b853cc93f02d13a2425ca464fe5e21f2195bd4
Ulrich Schmidt <admin@deruli.de>
2016-03-16 19:58:45 +0100

    shariff added

---
+++ sources/shariff-5.2.0/src/content/modules/shariff/shariff_footer.php
@@ -0,0 +1,12 @@
+<?php
+$data = get_custom_data ();
+$language = getCurrentLanguage ();
+if ($language != "de"){
+	$language = "en";
+};
+if (! isset ( $data ["disable_socialshare"] ) and ! isset ( $data ["disable_facebook_like"] ) and ! isset ( $data ["disable_google_plusone"] ) and is_200 ()) {
+	?>
+<script type="text/javascript"
+	src="<?php echo getModulePath("shariff");?>shariff.min.js"></script><?php
+}
+?>
---
+++ sources/shariff-5.2.0/src/content/modules/shariff/shariff_main.php
@@ -0,0 +1,4 @@
+<?php
+function shariff_render() {
+	return "";
+}
