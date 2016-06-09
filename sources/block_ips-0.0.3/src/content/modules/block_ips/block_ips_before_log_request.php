834e79c790f10930415e7acd1df6a4c1e7e6e9ac
Ulrich Schmidt <u.schmidt@velian.de>
2016-06-09 16:37:49 +0200

    block_ips-0.0.3

--- sources/block_ips-0.0.3/src/content/modules/block_ips/block_ips_before_log_request.php
+++ sources/block_ips-0.0.3/src/content/modules/block_ips/block_ips_before_log_request.php
@@ -3,13 +3,13 @@ function isIPBlocked($ip) {
 	$blocked_ips = getconfig ( "blocked_ips" );
 	if (! $blocked_ips)
 		return false;
-	
+
 	if (empty ( $blocked_ips ))
 		return false;
-	
+
 	$blocked_ips = str_replace ( "\r\n", "\n", $blocked_ips );
 	$blocked_ips = explode ( "\n", $blocked_ips );
-	
+
 	for($i = 0; $i < count ( $blocked_ips ); $i ++) {
 		if (endsWith ( $blocked_ips [$i], "." )) {
 			if (startsWith ( $ip, $blocked_ips [$i] ))
@@ -18,7 +18,7 @@ function isIPBlocked($ip) {
 			return true;
 		}
 	}
-	
+
 	return false;
 }
 
@@ -26,5 +26,5 @@ $ip = $_SERVER ['REMOTE_ADDR'];
 
 if (isIPBlocked ( $ip )) {
 	header ( 'HTTP/1.0 403 Forbidden' );
-	die ( "Der Zugriff von Ihrer IP-Adresse ($ip) wurde gesperrt!" );
+	die ( get_translation("access_from_your_ip_is_blocked", array("%ip%" => $ip)) );
 }
---
+++ sources/block_ips-0.0.3/src/content/modules/block_ips/block_ips_lang_de.php
@@ -0,0 +1,7 @@
+<?php
+define("TRANSLATION_BLOCK_IP_ADRESSES", "IP-Adressen blockieren");
+define("TRANSLATION_BLOCK_IP_HELP_TEXT", "Hier können Sie den Zugriff von bestimmten IP-Adressen auf das System
+blockieren.<br />Sie können je einen Eintrag pro Zeile eingeben.<br />
+Wenn eine Zeile mit einem Punkt endet, werden alle IP-Adressen die
+damit anfangen blockiert.");
+define("TRANSLATION_ACCESS_FROM_YOUR_IP_IS_BLOCKED", "Der Zugriff von Ihrer IP-Adresse (%ip%) wurde gesperrt!");
