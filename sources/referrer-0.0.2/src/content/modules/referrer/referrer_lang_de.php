fbdf7ac7e857c783fdde0d427543c414a4c14fc1
Ulrich Schmidt <ulrich1992@web.de>
2016-06-26 13:05:33 +0200

    refakturiert

--- sources/referrer-0.0.2/src/content/modules/referrer/referrer_lang_de.php
+++ sources/referrer-0.0.2/src/content/modules/referrer/referrer_lang_de.php
@@ -1,3 +1,3 @@
 <?php
-define("TRANSLATION_REFERRER_HEADLINE", "Häufige Referrer" );
-define("TRANSLATION_REFERRER_NO_DATA", "Bisher noch keine Referrer vorhanden." );
+define ( "TRANSLATION_REFERRER_HEADLINE", "Häufige Referrer" );
+define ( "TRANSLATION_REFERRER_NO_DATA", "Bisher noch keine Referrer vorhanden." );
