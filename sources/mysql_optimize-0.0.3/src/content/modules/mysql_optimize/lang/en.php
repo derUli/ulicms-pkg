119d58da170ac4bb2177f3e35be305a1ae5feeb7
Ulrich Schmidt <u.schmidt@velian.de>
2016-06-28 12:59:19 +0200

    mysql_optimize übersetzt

---
+++ sources/mysql_optimize-0.0.3/src/content/modules/mysql_optimize/lang/en.php
@@ -0,0 +1,5 @@
+<?php
+define ( "TRANSLATION_OPTIMIZE_DATABASE", "Optimize Database" );
+define ( "TRANSLATION_OPTIMIZE_AND_UPDATE_DATABASE", "Optimize and repair Database " );
+define ( "TRANSLATION_OK", "OK" );
+define ( "TRANSLATION_FAILED", "Failed" );
