fd2bc0f1a93b738371d4b98d37ff94873a7d8a82
Ulrich Schmidt <u.schmidt@velian.de>
2016-06-30 15:43:24 +0200

    typo fixed

--- sources/update_manager-1.0.1/src/content/modules/update_manager/lang/de.php
+++ sources/update_manager-1.0.1/src/content/modules/update_manager/lang/de.php
@@ -2,7 +2,7 @@
 define ( "TRANSLATION_UPDATE_MANAGER", "Update Manager" );
 define ( "TRANSLATION_INSTALL_UPDATES", "Installiere Updates" );
 define ( "TRANSLATION_PLEASE_SELECT_PACKAGES", "Bitte wählen Sie Pakete zum aktualisieren aus." );
-define ( "TRANSLATION_NO_UPDATES_AVAILABLE", "Keine Updates verfügbar." );
 
+define ( "TRANSLATION_NO_UPDATES_AVAILABLE", "Keine Updates verfügbar." );
 define ( "TRANSLATION_UPDATES_AVAILABLE_SINGULAR", "Es ist ein Update verfügbar." );
-define ( "TRANSLATION_UPDATES_AVAILABLE_MULTI", "Es sind %count% Updates verfügbar." );
+define ( "TRANSLATION_UPDATES_AVAILABLE_MULTIPLE", "Es sind %count% Updates verfügbar." );
