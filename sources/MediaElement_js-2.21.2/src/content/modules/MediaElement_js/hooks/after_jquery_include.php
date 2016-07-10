36e11216fb3b9365c85f61da91d52e9ebb138b90
Ulrich Schmidt <admin@deruli.de>
2016-07-10 10:23:34 +0200

    mediaelement.js hinzugefügt

---
+++ sources/MediaElement_js-2.21.2/src/content/modules/MediaElement_js/hooks/after_jquery_include.php
@@ -0,0 +1,6 @@
+<script type="text/javascript"
+	src="<?php echo getModulePath("MediaElement_js")?>ressources/mediaelement-and-player.min.js"></script>
+<link rel="stylesheet"
+	href="<?php echo getModulePath("MediaElement_js")?>ressources/mediaelementplayer.min.css" />
+<script type="text/javascript"
+	src="<?php echo getModulePath("MediaElement_js")?>replace-tags.js"></script>
---
+++ sources/MediaElement_js-2.21.2/src/content/modules/MediaElement_js/replace-tags.js
@@ -0,0 +1,4 @@
+$(function() {
+	// using jQuery
+	$('video, audio').mediaelementplayer();
+});
