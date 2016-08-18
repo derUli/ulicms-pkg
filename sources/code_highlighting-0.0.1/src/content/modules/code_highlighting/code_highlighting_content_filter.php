9ef5b7ad886bc36b7068983296c2961f874e3e02
Ulrich Schmidt <u.schmidt@velian.de>
2016-08-18 12:17:19 +0200

    blog-6.4.9: Apply content_filter to text

--- sources/code_highlighting-0.0.1/src/content/modules/code_highlighting/code_highlighting_content_filter.php
+++ sources/code_highlighting-0.0.1/src/content/modules/code_highlighting/code_highlighting_content_filter.php
@@ -1,23 +1,24 @@
 <?php
 function code_highlighting_content_filter($html) {
 	// Wenn der Content keinen code-Tag enthält, abbrechen
-	if (strpos ( $html, "[code src=" ) === false)
+	if (strpos ( $html, "[code src=" ) === false){
 		return $html;
-	
+	}
+
 	$baseDir = "./content/files/";
 	$allContentFiles = find_all_files ( $baseDir );
-	
+
 	// Durch den files-Ordner durchiterieren
 	for($l = 0; $l < count ( $allContentFiles ); $l ++) {
 		$replaceName = str_replace ( $baseDir, "", $allContentFiles [$l] );
 		$replaceName = str_replace ( "../", "", $replaceName );
 		$replaceName = trim ( $replaceName, "/" );
 		$replaceName = html_entity_decode ( $replaceName, ENT_QUOTES, "UTF-8" );
-		
+
 		// Zu ersetzender String mit kodierten Anführungszeichen und ohne
 		$replaceString1 = "[code src=\"" . $replaceName . "\"]";
 		$replaceString2 = "[code src=&quot;" . $replaceName . "&quot;]";
-		
+
 		$mime = get_mime ( $allContentFiles [$l] );
 		$allowed_mime = array (
 				"text/php",
@@ -30,14 +31,14 @@ function code_highlighting_content_filter($html) {
 				"text/html",
 				"text/xml",
 				"text/javascript",
-				"application/json" 
+				"application/json"
 		);
-		
+
 		if (in_array ( $mime, $allowed_mime )) {
-			
+
 			// Code in String lesen
 			$fileContent = highlight_file ( $allContentFiles [$l], true );
-			
+
 			// Platzhalter ersetzen
 			$html = str_replace ( $replaceString1, $fileContent, $html );
 			$html = str_replace ( $replaceString2, $fileContent, $html );
