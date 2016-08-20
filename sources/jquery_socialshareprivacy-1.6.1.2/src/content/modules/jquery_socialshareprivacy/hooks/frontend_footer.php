64253d378f18505d95257734b37ec2c1336a9885
Ulrich Schmidt <admin@deruli.de>
2016-08-20 10:51:22 +0200

    jquery 1.6.1.2: Performance optimierung

--- sources/jquery_socialshareprivacy-1.6.1.2/src/content/modules/jquery_socialshareprivacy/hooks/frontend_footer.php
+++ sources/jquery_socialshareprivacy-1.6.1.2/src/content/modules/jquery_socialshareprivacy/hooks/frontend_footer.php
@@ -1,8 +1,9 @@
 <?php
 $data = get_custom_data ();
 $language = getCurrentLanguage ();
-if ($language != "de")
+if ($language != "de"){
 	$language = "en";
+}
 if (! isset ( $data ["disable_socialshare"] ) and ! isset ( $data ["disable_facebook_like"] ) and ! isset ( $data ["disable_google_plusone"] ) and is_200 ()) {
 	?>
 <script type="text/javascript"
