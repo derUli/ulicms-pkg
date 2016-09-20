13e54d1288c55cdee80d676a78dfbeb2eb391aff
Ulrich Schmidt <admin@deruli.de>
2016-09-20 22:06:53 +0200

    custom_admin_css modul hinzugefügt

---
+++ sources/custom_admin_css-0.0.2/src/content/modules/custom_admin_css/lang/de.php
@@ -0,0 +1,4 @@
+<?php
+define ( "TRANSLATION_CUSTOM_ADMIN_CSS_TITLE", "Benutzerdefinierte Admin CSS Regeln" );
+define ( "TRANSLATION_CUSTOM_ADMIN_CSS_HELP", "Hier können Sie eigene CSS Regeln definieren, ohne den Code der
+		Templates modifizieren zu müssen." );
---
+++ sources/custom_admin_css-0.0.2/src/content/modules/custom_admin_css/lang/en.php
@@ -0,0 +1,4 @@
+<?php
+define ( "TRANSLATION_CUSTOM_ADMIN_CSS_TITLE", "Custom Admin CSS Rules" );
+define ( "TRANSLATION_CUSTOM_ADMIN_CSS_HELP", "Here you can define your own CSS rules without having the code
+Templates need to modify." );
