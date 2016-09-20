e7f0b7dc24970e69977e8f13a900e69d92602fd9
Ulrich Schmidt <admin@deruli.de>
2016-09-20 22:09:11 +0200

    refactor

--- sources/custom_admin_css-0.0.2/src/content/modules/custom_admin_css/lang/de.php
+++ sources/custom_admin_css-0.0.2/src/content/modules/custom_admin_css/lang/de.php
@@ -1,4 +1,3 @@
 <?php
 define ( "TRANSLATION_CUSTOM_ADMIN_CSS_TITLE", "Benutzerdefinierte Admin CSS Regeln" );
-define ( "TRANSLATION_CUSTOM_ADMIN_CSS_HELP", "Hier können Sie eigene CSS Regeln definieren, ohne den Code der
-		Templates modifizieren zu müssen." );
+define ( "TRANSLATION_CUSTOM_ADMIN_CSS_HELP", "Hier können Sie eigene CSS Regeln definieren, um das Layout des Backends anzupassen." );
