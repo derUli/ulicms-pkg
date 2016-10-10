efb3b3f9c12dfe1cc6a8d3ae01d33df2fccedc18
Ulrich Schmidt <admin@deruli.de>
2016-10-10 15:40:37 +0200

    sprachdatei

--- sources/robots_txt_editor-1.0/src/content/modules/robots_txt_editor/lang/de.php
+++ sources/robots_txt_editor-1.0/src/content/modules/robots_txt_editor/lang/de.php
@@ -1,3 +1,3 @@
 <?php
 define ( "TRANSLATION_ROBOTS_TXT_EDITOR", "robots.txt Editor" );
-define ( "TRANSLATION_ABOUT_ROBOTS", "Mehr über robots.txt erfahren" );
+define ( "TRANSLATION_ABOUT_ROBOTS_TXT", "Mehr über robots.txt erfahren" );
