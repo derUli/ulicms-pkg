4313e46c8969349a5216aaa5a6ff98282cb108a9
Ulrich Schmidt <admin@deruli.de>
2016-10-28 11:45:40 +0200

    modul notify_on_registration hinzugefügt

---
+++ sources/notify_on_registration-0.9.1/src/content/modules/notify_on_registration/hook/after_user_registration.php
@@ -0,0 +1,12 @@
+<?php
+$to = getconfig ( "email" );
+$subject = get_translation ( "REGISTRATION_EMAIL_NOTIFICATION_SUBJECT", array (
+		"{domain}" => get_domain () 
+) );
+$message = get_translation ( "REGISTRATION_EMAIL_NOTIFICATION_MESSAGE", array (
+		"{domain}" => get_domain (),
+		"{username}" => $_POST ["admin_username"],
+		"{ip}" => get_ip () 
+) );
+
+ulicms_mail ( $to, $subject, $message );
---
+++ sources/notify_on_registration-0.9.1/src/content/modules/notify_on_registration/main.php
@@ -0,0 +1,4 @@
+<?php
+function notify_on_registration_render() {
+	return "";
+}
