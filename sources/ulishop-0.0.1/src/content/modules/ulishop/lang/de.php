c94cb050fa5f946c39d41c818e4c9e3391572d4f
Ulrich Schmidt <admin@deruli.de>
2016-11-15 23:05:48 +0100

    ulishop

---
+++ sources/ulishop-0.0.1/src/content/modules/ulishop/lang/de.php
@@ -0,0 +1,5 @@
+<?php
+define ( "TRANSLATION_PRODUCT", "Produkt" );
+define ( "TRANSLATION_PRICE", "Preis" );
+define ( "TRANSLATION_SPECIAL_TAX", "Spezieller Mehrwertsteuersatz (in Prozent)" );
+define ( "TRANSLATION_QUANTITY", "Menge bzw. Gewicht (mit Einheit)" );
---
+++ sources/ulishop-0.0.1/src/content/modules/ulishop/main.php
@@ -0,0 +1,4 @@
+<?php
+function ulishop_render() {
+	return "";
+}
