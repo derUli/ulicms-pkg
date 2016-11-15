2166e05ab45bc851716c4ff9c035d6cee30fa646
Ulrich Schmidt <admin@deruli.de>
2016-11-15 23:15:30 +0100

    basket checkout

--- sources/ulishop-0.0.1/src/content/modules/ulishop/lang/de.php
+++ sources/ulishop-0.0.1/src/content/modules/ulishop/lang/de.php
@@ -1,5 +1,11 @@
 <?php
-define ( "TRANSLATION_PRODUCT", "Produkt" );
+$TRANSLATION_ULISHOP_SEPERATOR = htmlspecialchars ( "->" );
+define ( "TRANSLATION_PRODUCT", "UliShop $TRANSLATION_ULISHOP_SEPERATOR Produkt" );
 define ( "TRANSLATION_PRICE", "Preis" );
 define ( "TRANSLATION_SPECIAL_TAX", "Spezieller Mehrwertsteuersatz (in Prozent)" );
 define ( "TRANSLATION_QUANTITY", "Menge bzw. Gewicht (mit Einheit)" );
+define ( "TRANSLATION_BASKET", "UliShop $TRANSLATION_ULISHOP_SEPERATOR Warenkorb" );
+define ( "TRANSLATION_CHECKOUT", "UliShop $TRANSLATION_ULISHOP_SEPERATOR Checkout" );
+
+define ( "TRANSLATION_THANK_YOU", "UliShop $TRANSLATION_ULISHOP_SEPERATOR Danke für Ihre Bestellung" );
+
