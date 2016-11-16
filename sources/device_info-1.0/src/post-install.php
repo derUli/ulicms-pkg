9a4e7011bad8015d4c64269b07a8b152a34662dd
Ulrich Schmidt <u.schmidt@velian.de>
2016-11-16 15:34:15 +0100

    typo fixed

--- sources/device_info-1.0/src/post-install.php
+++ sources/device_info-1.0/src/post-install.php
@@ -4,7 +4,7 @@ Database::query("CREATE TABLE IF NOT EXISTS `{prefix}device_infos` (
   `tablet` mediumint(9) NOT NULL DEFAULT '0',
   `crawler` mediumint(9) NOT NULL DEFAULT '0',
   `pc` mediumint(9) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=ut8;", true);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;", true);
 
 $query = Database::query("select * from {prefix}device_infos", true);
 if(Database::getNumRows($query) <= 0){
