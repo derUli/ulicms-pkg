c10fc66371d371615f7d4525587ed43dd6f9d7f2
Ulrich Schmidt <u.schmidt@velian.de>
2017-01-05 15:51:58 +0100

    xml sitemap 0.0.6, port aus https urls entfernt

---
+++ sources/xml_sitemap-0.0.6/src/content/modules/xml_sitemap/xml_sitemap_lang_de.php
@@ -0,0 +1,4 @@
+<?php
+define ( "TRANSLATION_GENERATE_XML_SITEMAP", "XML Sitemap generieren" );
+define ( "TRANSLATION_GENERATE_XML_SITEMAP_SUCCESS", "<p><a href=\"../sitemap.xml\" target=\"_blank\">sitemap.xml</a> wurde generiert.</p>" );
+define ( "TRANSLATION_GENERATE_XML_SITEMAP_FAILED", "<p>sitemap.xml konnte nicht erzeugt werden. Bitte legen Sie die Datei manuell an und fügen Sie folgenden Code ein.</p>" );
