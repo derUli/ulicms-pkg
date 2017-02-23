b60305527d2e3e71e36210cf0dc14d1e10272835
Ulrich Schmidt <admin@deruli.de>
2017-02-23 22:32:29 +0100

    google_maps Modul gibt nun den Key mit

--- sources/google_maps-0.0.4/src/content/modules/google_maps/google_maps_admin.php
+++ sources/google_maps-0.0.4/src/content/modules/google_maps/google_maps_admin.php
@@ -4,8 +4,8 @@ define ( "MODULE_ADMIN_REQUIRED_PERMISSION", "google_maps" );
 function google_maps_admin() {
 	if (isset ( $_POST ["submit"] )) {
 		setconfig ( "google_maps_marker", db_escape ( $_POST ["google_maps_marker"] ) );
-		
 		setconfig ( "google_maps_zoom_level", intval ( $_POST ["google_maps_zoom_level"] ) );
+		Settings::set ( "google_maps_api_key", $_POST ["google_maps_api_key"] );
 	}
 	
 	$google_maps_marker = getconfig ( "google_maps_marker" );
@@ -13,6 +13,7 @@ function google_maps_admin() {
 	$google_maps_zoom_level = getconfig ( "google_maps_zoom_level" );
 	$google_maps_zoom_level = intval ( $google_maps_zoom_level );
 	
+	$google_maps_api_key = Settings::get ( "google_maps_api_key" );
 	?>
 
 <form action="<?php echo getModuleAdminSelfPath()?>" method="post">
@@ -55,6 +56,12 @@ function google_maps_admin() {
 </select>
 	
 	
+	<p>
+		<strong><?php translate("google_maps_api_key");?></strong> <br /> <input
+			type="text" name="google_maps_api_key"
+			value="<?php Template::escape($google_maps_api_key);?>">
+	</p>
+
 	<p>
 		<input type="submit" name="submit" value="<?php translate("save");?>" />
 	</p>
--- sources/google_maps-0.0.4/src/post-install.php
+++ sources/google_maps-0.0.4/src/post-install.php
@@ -2,3 +2,4 @@
 Settings::register ( "google_maps_marker", "Am Flaschendreherkamp 12" );
 
 Settings::register ( "google_maps_zoom_level", "12" );
+Settings::register ( "google_maps_api_key", "InsertYourKeyHere" );
