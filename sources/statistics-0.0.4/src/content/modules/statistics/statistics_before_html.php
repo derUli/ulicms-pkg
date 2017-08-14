ca74c6f23d13b86fdb4b8dbeeb1448d673a6e768
Ulrich Schmidt <admin@deruli.de>
2017-08-14 18:34:28 +0200

    syntaxfehler bei statistics korrigiert

--- sources/statistics-0.0.4/src/content/modules/statistics/statistics_before_html.php
+++ sources/statistics-0.0.4/src/content/modules/statistics/statistics_before_html.php
@@ -1,18 +1,18 @@
 <?php
-if (! logged_in () and ! is_admin_dir () and ! is_crawler ()) {
-	
-	$heute = mktime ( 0, 0, 0, date ( "m" ), date ( "d" ), date ( "Y" ) );
-	
-	$visitorHash = md5 ( $_SERVER ["REMOTE_ADDR"] . $_SERVER ["HTTP_USER_AGENT"] );
-	
-	$query = db_query ( "SELECT id FROM " . tbname ( "statistics" ) . " WHERE hash='$visitorHash' AND `date` >= $heute" );
-	
-	if (db_num_rows ( $query ) > 0) {
-		db_query ( "UPDATE " . tbname ( "statistics" ) . " SET `date` = " . time () . ", `views` = `views` + 1 WHERE hash ='$visitorHash' AND `date` >= $heute" );
-	} else {
-		db_query ( "INSERT INTO " . tbname ( "statistics" ) . " (hash, date, `views`) VALUES ('$visitorHash',
-     " . time () . ", 1)" );
-	}
+if (! logged_in() and ! is_admin_dir() and ! is_crawler()) {
+    
+    $heute = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+    
+    $visitorHash = md5(get_ip() . get_useragent());
+    
+    $query = db_query("SELECT id FROM " . tbname("statistics") . " WHERE hash='$visitorHash' AND `date` >= $heute");
+    
+    if (db_num_rows($query) > 0) {
+        db_query("UPDATE " . tbname("statistics") . " SET `date` = " . time() . ", `views` = `views` + 1 WHERE hash ='$visitorHash' AND `date` >= $heute");
+    } else {
+        db_query("INSERT INTO " . tbname("statistics") . " (hash, date, `views`) VALUES ('$visitorHash',
+     " . time() . ", 1)");
+    }
 }
 
 ?>
