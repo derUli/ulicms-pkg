f03a54f0ef2c8bd5eea9b376c7e7b191776304a6
Ulrich Schmidt <admin@deruli.de>
2017-09-05 18:15:50 +0200

    updated font_awesome to version 4.7.0

---
+++ sources/font_awesome-4.7.0/src/content/modules/font_awesome/FontAwesome.php
@@ -0,0 +1,17 @@
+<?php
+
+class FontAwesome extends Controller
+{
+
+    private $moduleName = "font_awesome";
+
+    public function head()
+    {
+        echo Template::executeModuleTemplate($this->moduleName, "head.php");
+    }
+
+    public function admin_head()
+    {
+        $this->head();
+    }
+}
