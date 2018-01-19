a8bd4fd99c09d6092894321a6d71bd215f52582e
Ulrich Schmidt <admin@deruli.de>
2018-01-19 17:55:35 +0100

    phpmyadmin aktualisiert "

--- sources/sql_console-0.0.2/src/content/modules/sql_console/sql_console_form.php
+++ sources/sql_console-0.0.2/src/content/modules/sql_console/sql_console_form.php
@@ -1,11 +1,11 @@
 <?php
-if (! defined ( "MODULE_ADMIN_REQUIRED_PERMISSION" ))
-	die ( "Lam0r! ^^" );
+if (! defined("MODULE_ADMIN_REQUIRED_PERMISSION"))
+    die("Lam0r! ^^");
 ?>
 <form action="<?php echo getModuleAdminSelfPath()?>" method="post">
 <?php
 
-csrf_token_html ();
+csrf_token_html();
 ?>
 <p>
 		<textarea name="sql_code" id="sql_code"><?php echo htmlspecialchars($_SESSION["sql_code"])?></textarea>
