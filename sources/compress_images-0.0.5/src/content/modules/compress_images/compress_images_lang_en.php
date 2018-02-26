644c0f33653a0e128a82dba042629c0c9d891956
Ulrich Schmidt <admin@deruli.de>
2018-02-26 10:32:35 +0100

    compress_images entfernt

--- sources/compress_images-0.0.5/src/content/modules/compress_images/compress_images_lang_en.php
+++
@@ -1,7 +0,0 @@
-<?php
-define ( "TRANSLATION_IMAGE_COMPRESSION", "Image Compression" );
-define ( "TRANSLATION_OLD_SIZE", "Old Size: %size%" );
-define ( "TRANSLATION_NEW_SIZE", "New Size: %size%" );
-define ( "TRANSLATION_RATIO", "Ratio: %ratio1% % (%ratio2%)" );
-define ( "TRANSLATION_FINISHED", "Finished" );
-define ( "TRANSLATION_DO_COMPRESSION", "Do Compression" );
