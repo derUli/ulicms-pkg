a89d8b1e1ae15cf315006d09cbad98dcceb81bf4
Ulrich Schmidt <support@ulicms.de>
2018-06-10 21:54:54 +0200

    lightbox_gallery metadata file fixed

--- sources/newsletter-0.1.1/src/content/modules/newsletter/newsletter_form.php
+++ sources/newsletter-0.1.1/src/content/modules/newsletter/newsletter_form.php
@@ -1,54 +1,54 @@
 <?php
-$acl = new ACL ();
-if (! $acl->hasPermission ( "newsletter" )) {
-	die ( "Access denied!" );
+$acl = new ACL();
+if (! $acl->hasPermission("newsletter")) {
+    die("Access denied!");
 }
 
-if (isset ( $_POST ["submit"] )) {
-	$_SESSION ["newsletter_data"] ["newsletter_text"] = $_POST ["newsletter_text"];
-	
-	$servername = $_SERVER ["SERVER_NAME"];
-	
-	if ($_SERVER ['HTTPS']) {
-		$url = "https://";
-	} else {
-		$url = "http://";
-	}
-	
-	$url .= $servername;
-	
-	// Replace Relative URLs
-	$_SESSION ["newsletter_data"] ["newsletter_text"] = str_replace ( "<a href=\"/", "<a href=\"$url/", $_SESSION ["newsletter_data"] ["newsletter_text"] );
-	
-	$_SESSION ["newsletter_data"] ["newsletter_text"] = str_replace ( "<a href='/", "<a href='$url/", $_SESSION ["newsletter_data"] ["newsletter_text"] );
-	
-	$_SESSION ["newsletter_data"] ["newsletter_text"] = str_replace ( " src=\"/", " src=\"$url/", $_SESSION ["newsletter_data"] ["newsletter_text"] );
-	
-	$_SESSION ["newsletter_data"] ["newsletter_text"] = str_replace ( " href=\"?seite=", " href=\"$url/?seite=", $_SESSION ["newsletter_data"] ["newsletter_text"] );
-	
-	$_SESSION ["newsletter_data"] ["newsletter_title"] = $_POST ["newsletter_title"];
-	
-	$send_to = $_POST ["send_to"];
-	
-	if ($send_to == "ALL") {
-		$subscribers = getSubscribers ();
-	} else {
-		$subscribers = array (
-				$send_to 
-		);
-	}
-	$_SESSION ["newsletter_data"] ["newsletter_receivers"] = $subscribers;
-	$_SESSION ["newsletter_data"] ["newsletter_remaining"] = count ( $subscribers );
+if (isset($_POST["submit"])) {
+    $_SESSION["newsletter_data"]["newsletter_text"] = $_POST["newsletter_text"];
+    
+    $servername = $_SERVER["SERVER_NAME"];
+    
+    if ($_SERVER['HTTPS']) {
+        $url = "https://";
+    } else {
+        $url = "http://";
+    }
+    
+    $url .= $servername;
+    
+    // Replace Relative URLs
+    $_SESSION["newsletter_data"]["newsletter_text"] = str_replace("<a href=\"/", "<a href=\"$url/", $_SESSION["newsletter_data"]["newsletter_text"]);
+    
+    $_SESSION["newsletter_data"]["newsletter_text"] = str_replace("<a href='/", "<a href='$url/", $_SESSION["newsletter_data"]["newsletter_text"]);
+    
+    $_SESSION["newsletter_data"]["newsletter_text"] = str_replace(" src=\"/", " src=\"$url/", $_SESSION["newsletter_data"]["newsletter_text"]);
+    
+    $_SESSION["newsletter_data"]["newsletter_text"] = str_replace(" href=\"?seite=", " href=\"$url/?seite=", $_SESSION["newsletter_data"]["newsletter_text"]);
+    
+    $_SESSION["newsletter_data"]["newsletter_title"] = $_POST["newsletter_title"];
+    
+    $send_to = $_POST["send_to"];
+    
+    if ($send_to == "ALL") {
+        $subscribers = getSubscribers();
+    } else {
+        $subscribers = array(
+            $send_to
+        );
+    }
+    $_SESSION["newsletter_data"]["newsletter_receivers"] = $subscribers;
+    $_SESSION["newsletter_data"]["newsletter_remaining"] = count($subscribers);
 }
 
-$user = getUserById ( $_SESSION ["login_id"] );
-$email = $user ["email"];
+$user = getUserById($_SESSION["login_id"]);
+$email = $user["email"];
 
 ?>
 <h3>Newsletter vorbereiten</h3>
 <?php
-if (isset ( $_POST ["submit"] )) {
-	?>
+if (isset($_POST["submit"])) {
+    ?>
 <script type="text/javascript">
 url = window.location.href.toString()
 window.location.replace(url);
@@ -60,10 +60,10 @@ window.location.replace(url);
 <form method="post" action="<?php echo getModuleAdminSelfPath()?>">
 <?php
 
-csrf_token_html ();
+csrf_token_html();
 ?>
 <p>
-		<strong>Titel: </strong><input type="text" maxlength=78 size=78
+		<strong>Titel: </strong><input type="text" maxlength="78" size="78"
 			name="newsletter_title"
 			value="<?php echo htmlspecialchars($_SESSION["newsletter_data"]["newsletter_title"])?>">
 	</p>
@@ -71,16 +71,16 @@ csrf_token_html ();
 
 	<p>
 		<textarea id="newsletter_text" name="newsletter_text" cols=60 rows=16><?php
-		echo htmlspecialchars ( $_SESSION ["newsletter_data"] ["newsletter_text"] );
-		?></textarea>
+echo htmlspecialchars($_SESSION["newsletter_data"]["newsletter_text"]);
+?></textarea>
 	</p>
 	<script type="text/javascript">
 var editor = CKEDITOR.replace( 'newsletter_text',
 					{
 						skin : '<?php
-						
-						echo getconfig ( "ckeditor_skin" );
-						?>'
+    
+    echo getconfig("ckeditor_skin");
+    ?>'
 					});                                         
 
 </script>
@@ -90,15 +90,15 @@ var editor = CKEDITOR.replace( 'newsletter_text',
 		<input name="send_to" type="radio" checked
 			value="<?php echo getconfig("email")?>"> Testmail an <?php echo getconfig("email")?><br />
 <?php
-if ($email != getconfig ( "email" )) {
-	?>
+if ($email != getconfig("email")) {
+    ?>
 <input name="send_to" type="radio" value="<?php
-	
-	echo $email;
-	?>"> Testmail an <?php
-	
-	echo $email;
-	?><br />
+    
+    echo $email;
+    ?>"> Testmail an <?php
+    
+    echo $email;
+    ?><br />
 <?php
 }
 ?>
