b5649b517ea3b428118905dfe95e03723a9e9d81
Ulrich Schmidt <admin@deruli.de>
2019-05-01 17:43:01 +0200

    Update better_touch_after_jquery_include.php

--- sources/better_touch-0.0.5/src/content/modules/better_touch/better_touch_after_jquery_include.php
+++ sources/better_touch-0.0.5/src/content/modules/better_touch/better_touch_after_jquery_include.php
@@ -5,6 +5,6 @@ echo getModulePath ( "better_touch" );
 	?>doubletaptogo.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
-$(".menu_top li:has(ul)").doubleTapToGo();
+	$(".menu_top li:has(ul)").doubleTapToGo();
 })
 </script>
