ecc8fb4d444209473255ed34a7919fcfc1acdd35
Ulrich Schmidt <admin@deruli.de>
2019-08-06 00:02:47 +0200

    remove google_plusone (since Google+ is dead)

--- sources/google_plusone-0.0.2/src/content/modules/google_plusone/google_plusone_head.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = get_custom_data ();
-if (! isset ( $data ["disable_google_plusone"] ) and is_200 ()) {
-	?>
-<script type="text/javascript"
-	src="https://apis.google.com/js/plusone.js"></script>
-<?php
-
-}
