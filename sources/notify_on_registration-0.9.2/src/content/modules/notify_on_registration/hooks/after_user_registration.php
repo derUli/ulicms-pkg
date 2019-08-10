fe28136e3b8572bbddd2fc0201e4a2526ff2e1f6
Ulrich Schmidt <admin@deruli.de>
2019-08-10 11:34:27 +0200

    notifiy_on_registration show user name again

--- sources/notify_on_registration-0.9.2/src/content/modules/notify_on_registration/hooks/after_user_registration.php
+++ sources/notify_on_registration-0.9.2/src/content/modules/notify_on_registration/hooks/after_user_registration.php
@@ -1,11 +1,11 @@
 <?php
-$to = getconfig ( "email" );
+$to = Settings::get ( "email" );
 $subject = get_translation ( "REGISTRATION_EMAIL_NOTIFICATION_SUBJECT", array (
 		"{domain}" => get_domain () 
 ) );
 $message = get_translation ( "REGISTRATION_EMAIL_NOTIFICATION_MESSAGE", array (
 		"{domain}" => get_domain (),
-		"{username}" => $_POST ["admin_username"],
+		"{username}" => $_POST ["username"],
 		"{ip}" => get_ip () 
 ) );
 
