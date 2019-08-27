36de72204869aa35ddf1733824544aaee6779e05
Ulrich Schmidt <admin@deruli.de>
2019-08-27 18:13:07 +0200

    update faq package, remove some obsolete packages

--- sources/fullcalendar-0.1.6/src/content/modules/fullcalendar/lang/en.php
+++
@@ -1,6 +0,0 @@
-<?php
-define ( "TRANSLATION_BEGIN", "Begin:" );
-define ( "TRANSLATION_END", "End:" );
-define ( "TRANSLATION_URL", "URL:" );
-define ( "TRANSLATION_ASK_FOR_DELETE_PROJECT", "Do you wanna delete this event?" );
-define ( "TRANSLATION_ADD_EVENT", "Add Event" );
