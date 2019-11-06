982267c0fa4a016dd0aa412c9fabdc5e948954d5
Ulrich Schmidt <admin@deruli.de>
2019-11-06 11:51:31 +0100

    blog: implement rtl check

--- sources/blog-6.8.0/src/content/modules/blog/blog_remove.php
+++ sources/blog-6.8.0/src/content/modules/blog/blog_remove.php
@@ -1,17 +1,19 @@
 <?php
+
 function blog_remove_post($post_id) {
 	$acl = new ACL ();
-	if ($acl->hasPermission ( "blog" )) {
-		db_query ( "DELETE FROM `" . tbname ( "blog" ) . "` WHERE id = $post_id" );
+	if ($acl->hasPermission("blog")) {
+		db_query("DELETE FROM `" . tbname("blog") . "` WHERE id = $post_id");
 		return "<p>Der Blogpost wurde erfolgreich gelöscht!</p>";
 	} else {
 		return "<p>Zugriff verweigert!</p>";
 	}
 }
+
 function blog_remove_comment($post_id) {
 	$acl = new ACL ();
-	if ($acl->hasPermission ( "blog" )) {
-		db_query ( "DELETE FROM `" . tbname ( "blog_comments" ) . "` WHERE id = $post_id" );
+	if ($acl->hasPermission("blog")) {
+		db_query("DELETE FROM `" . tbname("blog_comments") . "` WHERE id = $post_id");
 		return "<script type='text/javascript'>
 	   history.back();
 	   </script>";
@@ -19,4 +21,5 @@ function blog_remove_comment($post_id) {
 		return "<p>Zugriff verweigert</p>";
 	}
 }
+
 ?>
