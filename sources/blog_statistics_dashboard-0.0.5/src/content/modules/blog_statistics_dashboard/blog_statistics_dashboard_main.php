c3e61b5bfe9acbc22d8e75439beb97867eae41ea
Der Uli im Netz <admin@deruli.de>
2020-03-29 19:18:47 +0200

    update pacakges

---
+++ sources/blog_statistics_dashboard-0.0.5/src/content/modules/blog_statistics_dashboard/blog_statistics_dashboard_main.php
@@ -0,0 +1,11 @@
+<?php
+function blog_statistics_dashboard_render() {
+	$mainFile = getModuleMainFilePath ( "blog" );
+	if (file_exists ( $mainFile )) {
+		include $mainFile;
+		return blog_render ();
+	}
+	
+	return "<p class=\"ulicms-error\">$mainFile fehlt!</p>";
+}
+?>
