4076c5bdcecf3e8e4020d3931919347911d603f7
Der Uli im Netz <admin@deruli.de>
2020-03-29 19:09:29 +0200

    make it 2020.2 compatible

---
+++ sources/theme-cleanblue-2.0.5/src/content/templates/cleanblue/unten.php
@@ -0,0 +1,23 @@
+<section class="advertisement">
+<?php
+
+random_banner ();
+?>
+</section>
+</main>
+<footer>
+<?php
+
+poweredByUliCMS ();
+?> | &copy; <?php
+
+year ();
+?> <?php
+
+homepage_owner ();
+?>
+</footer>
+</div>
+<?php Template::footer();?>
+</body>
+</html>
