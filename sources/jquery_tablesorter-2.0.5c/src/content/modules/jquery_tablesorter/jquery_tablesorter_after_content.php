23a35959b5435c48f4b4e3343d6d46277edd445b
Der Uli im Netz <admin@deruli.de>
2020-05-25 15:13:02 +0200

    upgrade some packages for compatiblity with UliCMS 2020.3

---
+++ sources/jquery_tablesorter-2.0.5c/src/content/modules/jquery_tablesorter/jquery_tablesorter_after_content.php
@@ -0,0 +1,15 @@
+<?php
+if (containsModule ( get_slug (), "jquery_tablesorter" )) {
+	?>
+<script type="text/javascript">
+$(document).ready(function() 
+    { 
+        $("table").tablesorter(); 
+    } 
+); 
+    
+</script>
+
+<?php
+}
+?>
