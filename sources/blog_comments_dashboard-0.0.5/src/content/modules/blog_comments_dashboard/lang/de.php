01bfd934321f70597c0a353675215152fceb0c6f
Uli <admin@deruli.de>
2022-03-06 21:07:27 +0100

    Blog Pakete entfernt

--- sources/blog_comments_dashboard-0.0.5/src/content/modules/blog_comments_dashboard/lang/de.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("TRANSLATION_NEW_COMMENTS", "Neue Kommentare");
-define("TRANSLATION_NO_COMMENTS", "Es sind noch keine Kommentare vorhanden.");
-define("TRANSLATION_REALLY_DELETE_COMMENT", "Möchten Sie diesen Kommentar wirklich löschen?");
--- sources/blog_statistics_dashboard-0.0.5/src/content/modules/blog_statistics_dashboard/lang/de.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("TRANSLATION_BLOG_STATISTICS", "Blog Statistiken");
-define("TRANSLATION_AMOUNT_OF_BLOG_POSTS", "Anzahl der Blogposts:");
-define("TRANSLATION_AMOUNT_OF_BLOG_COMMENTS", "Anzahl der Kommentare:");
-define("TRANSLATION_MOST_POPULAR_BLOG_POSTS", "Beliebteste Blogartikel");
-define("TRANSLATION_NO_BLOG_POSTS_THERE_YET", "Es sind noch keine Blogartikel vorhanden.");
