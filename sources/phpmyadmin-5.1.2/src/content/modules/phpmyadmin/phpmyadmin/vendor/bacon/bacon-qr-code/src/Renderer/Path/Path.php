1ee1cf215f5fa8abb57469d657ef377d3b957f93
Uli <admin@deruli.de>
2022-03-22 18:06:33 +0100

    Update Path.php

--- sources/phpmyadmin-5.1.2/src/content/modules/phpmyadmin/phpmyadmin/vendor/bacon/bacon-qr-code/src/Renderer/Path/Path.php
+++ sources/phpmyadmin-5.1.2/src/content/modules/phpmyadmin/phpmyadmin/vendor/bacon/bacon-qr-code/src/Renderer/Path/Path.php
@@ -1,5 +1,6 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
 
 namespace BaconQrCode\Renderer\Path;
 
@@ -9,8 +10,8 @@ use Traversable;
 /**
  * Internal Representation of a vector path.
  */
-final class Path implements IteratorAggregate
-{
+final class Path implements IteratorAggregate {
+
     /**
      * @var OperationInterface[]
      */
@@ -19,8 +20,7 @@ final class Path implements IteratorAggregate
     /**
      * Moves the drawing operation to a certain position.
      */
-    public function move(float $x, float $y) : self
-    {
+    public function move(float $x, float $y): self {
         $path = clone $this;
         $path->operations[] = new Move($x, $y);
         return $path;
@@ -29,8 +29,7 @@ final class Path implements IteratorAggregate
     /**
      * Draws a line from the current position to another position.
      */
-    public function line(float $x, float $y) : self
-    {
+    public function line(float $x, float $y): self {
         $path = clone $this;
         $path->operations[] = new Line($x, $y);
         return $path;
@@ -40,14 +39,14 @@ final class Path implements IteratorAggregate
      * Draws an elliptic arc from the current position to another position.
      */
     public function ellipticArc(
-        float $xRadius,
-        float $yRadius,
-        float $xAxisRotation,
-        bool $largeArc,
-        bool $sweep,
-        float $x,
-        float $y
-    ) : self {
+            float $xRadius,
+            float $yRadius,
+            float $xAxisRotation,
+            bool $largeArc,
+            bool $sweep,
+            float $x,
+            float $y
+    ): self {
         $path = clone $this;
         $path->operations[] = new EllipticArc($xRadius, $yRadius, $xAxisRotation, $largeArc, $sweep, $x, $y);
         return $path;
@@ -56,8 +55,7 @@ final class Path implements IteratorAggregate
     /**
      * Draws a curve from the current position to another position.
      */
-    public function curve(float $x1, float $y1, float $x2, float $y2, float $x3, float $y3) : self
-    {
+    public function curve(float $x1, float $y1, float $x2, float $y2, float $x3, float $y3): self {
         $path = clone $this;
         $path->operations[] = new Curve($x1, $y1, $x2, $y2, $x3, $y3);
         return $path;
@@ -66,8 +64,7 @@ final class Path implements IteratorAggregate
     /**
      * Closes a sub-path.
      */
-    public function close() : self
-    {
+    public function close(): self {
         $path = clone $this;
         $path->operations[] = Close::instance();
         return $path;
@@ -76,15 +73,13 @@ final class Path implements IteratorAggregate
     /**
      * Appends another path to this one.
      */
-    public function append(self $other) : self
-    {
+    public function append(self $other): self {
         $path = clone $this;
         $path->operations = array_merge($this->operations, $other->operations);
         return $path;
     }
 
-    public function translate(float $x, float $y) : self
-    {
+    public function translate(float $x, float $y): self {
         $path = new self();
 
         foreach ($this->operations as $operation) {
@@ -97,10 +92,10 @@ final class Path implements IteratorAggregate
     /**
      * @return OperationInterface[]|Traversable
      */
-    public function getIterator() : Traversable
-    {
+    public function getIterator(): Traversable {
         foreach ($this->operations as $operation) {
             yield $operation;
         }
     }
+
 }
