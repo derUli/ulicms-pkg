39072b0ad74e1b81d11bbcfc315d64eea73023c1
Uli <admin@deruli.de>
2023-02-20 18:07:52 +0100

    Ungetestete Pakete in Ordner verschoben

--- sources/slicknav-1.0.10/src/content/modules/slicknav/hooks/after_jquery_include.php
+++ sources/slicknav-1.0.10/src/content/modules/slicknav/hooks/after_jquery_include.php
@@ -1,9 +1,9 @@
-<script type="text/javascript"
-        src="<?php
-        echo getModulePath("slicknav");
-        ?>dist/jquery.slicknav.min.js?version=<?php echo getModuleMeta("slicknav", "version"); ?>"></script>
+<script
+    src="<?php
+    echo getModulePath("slicknav");
+    ?>dist/jquery.slicknav.min.js?version=<?php echo getModuleMeta("slicknav", "version"); ?>"></script>
 <link type="text/css"
       href="<?php
       echo getModulePath("slicknav");
-      ?>dist/slicknav.min.css?version=<?php echo getModuleMeta("slicknav", "version"); ?>"
+    ?>dist/slicknav.min.css?version=<?php echo getModuleMeta("slicknav", "version"); ?>"
       rel="stylesheet" />
