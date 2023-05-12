4d5a8945e847706185fb2d38aed72f8335fa298b
Ulrich Schmidt <support@ulicms.de>
2023-05-12 23:43:48 +0200

    update manager

---
+++ sources/update_manager-1.1.4/src/content/modules/update_manager/lang/de.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+defined('ULICMS_ROOT') || exit('No direct script access allowed');
+
+add_translation('UPDATE_MANAGER', 'Update Manager');
+add_translation('INSTALL_UPDATES', 'Installiere Updates');
+add_translation('PLEASE_SELECT_PACKAGES', 'Bitte wählen Sie Pakete zum aktualisieren aus.');
+
+add_translation('NO_UPDATES_AVAILABLE', 'Keine Updates verfügbar.');
+add_translation('UPDATES_AVAILABLE_SINGULAR', 'Es ist ein Update verfügbar.');
+add_translation('UPDATES_AVAILABLE_MULTIPLE', 'Es sind %count% Updates verfügbar.');
--- sources/update_manager-1.1.4/src/content/modules/update_manager/objects/update_manager.php
+++ sources/update_manager-1.1.4/src/content/modules/update_manager/objects/update_manager.php
@@ -2,36 +2,36 @@
 
 declare(strict_types=1);
 
+defined('ULICMS_ROOT') || exit('No direct script access allowed');
+
 use App\Packages\PackageManager;
 
-class UpdateManager
-{
-    public static function getAllUpdateablePackages(): array
-    {
+class UpdateManager {
+    public static function getAllUpdateablePackages(): array {
         $pkg = new PackageManager();
         $retval = [];
         $modules = getAllModules();
         foreach ($modules as $module) {
-            $version = getModuleMeta($module, "version");
+            $version = getModuleMeta($module, 'version');
             if ($version == null) {
                 continue;
             }
             $status = $pkg->checkForNewerVersionOfPackage($module);
-            if ($status and \App\Utils\VersionComparison::compare($status, $version, '>')) {
-                $retval[] = $module . "-" . $status;
+            if ($status && \App\Utils\VersionComparison::compare($status, $version, '>')) {
+                $retval[] = $module . '-' . $status;
             }
         }
 
         $themes = getAllThemes();
         foreach ($themes as $theme) {
-            $version = getThemeMeta($theme, "version");
+            $version = getThemeMeta($theme, 'version');
             if ($version == null) {
                 continue;
             }
-            $theme = "theme-" . $theme;
+            $theme = 'theme-' . $theme;
             $status = $pkg->checkForNewerVersionOfPackage($theme);
-            if ($status and \App\Utils\VersionComparison::compare($status, $version, '>')) {
-                $retval[] = $theme . "-" . $status;
+            if ($status && \App\Utils\VersionComparison::compare($status, $version, '>')) {
+                $retval[] = $theme . '-' . $status;
             }
         }
 
